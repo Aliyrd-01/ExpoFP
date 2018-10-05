1f1b5316118c849bcc184ce7ce6a1e5f726345d3
Evgeny Tukin <evgeny.tukin@9rays.net>
2018-10-05 20:04:38 +0300

    -

--- expos/demo/define.js
+++ expos/demo/define.js
@@ -1,5 +1,6 @@
 module.exports = {
     EFP_TITLE: JSON.stringify('ExpoFP demo'),
     EFP_HOME_URL: JSON.stringify('https://expofp.com/'),
-    EFP_LOGO_URL: JSON.stringify('INCNA18_360x180_Logo.png')
+    EFP_LOGO_URL: JSON.stringify('INCNA18_360x180_Logo.png'),
+    GTAG: JSON.stringify('UA-857963-22')
 };
