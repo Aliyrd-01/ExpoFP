21988b09193b350e51b898bfacc2a3644a8b2e04
Evgeny Tukin <evgeny.tukin@9rays.net>
2018-10-30 12:24:56 +0300

    Make triangle drawn

--- src/components/Map/draw.ts
+++ src/components/Map/draw.ts
@@ -3,18 +3,67 @@ import { svgWidth, svgHeight } from '@/tools/svg'
 // import c from './drawing-context'
 // import { drawFpGrid, drawDebug } from './draw-debug';
 import settings from '@/settings';
+import { createShader, createProgram } from './utils';
 // import { drawSprites } from './draw-sprites';
 // export { getBoothIdFromClientXy } from './booth-by-xy';
 
 let canvas: HTMLCanvasElement;
-let context: WebGLRenderingContext;
+let gl: WebGLRenderingContext;
+
 
 export function initialize(canvasParam: HTMLCanvasElement) {
     // draw all booths for now
     canvas = canvasParam;
-    context = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    sizeCanvases();
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 
     // define some shader programs, etc
 
-    
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, `attribute vec4 a_position;    
+    void main() {
+        gl_Position = a_position;
+    }`);
+
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, `precision mediump float;
+    void main() {
+        gl_FragColor = vec4(1, 0, 0.5, 1);
+    }`);
+
+    const program = createProgram(gl, vertexShader, fragmentShader);
+
+    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    const positions = [
+        0, 0,
+        0.5, 1,
+        1, 0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    gl.useProgram(program);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+function sizeCanvases() {
+    const bWidth = canvas.parentElement.clientWidth;
+    const bHeight = canvas.parentElement.clientHeight;
+    const cWidth = bWidth * devicePixelRatio;
+    const cHeight = bHeight * devicePixelRatio;
+
+    if (canvas.clientWidth !== bWidth || canvas.clientHeight !== bHeight) {
+        console.log('Setting canvas style width/height');
+
+        canvas.style.width = bWidth + 'px';
+        canvas.style.height = bHeight + 'px';
+    }
+
+    if (cWidth !== canvas.width || cHeight !== canvas.height) {
+        canvas.width = cWidth;
+        canvas.height = cHeight;
+    }
 }
