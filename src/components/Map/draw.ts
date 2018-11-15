b172f9d51d07f1b9c57977ffa09842ebd3698a8a
Evgeny Tukin <evgeny.tukin@9rays.net>
2018-11-15 16:13:03 +0300

    -

--- src/components/Map/draw.ts
+++ src/components/Map/draw.ts
@@ -15,6 +15,10 @@ const height = svgHeight;
 let gl: WebGLRenderingContext;
 let boothsProgramInfo: any;
 let boothsBufferInfo: any;
+let textProgramInfo: any;
+let textBufferInfo: any;
+let textUniforms: any;
+let textWidth, textHeight;
 
 let zoomTranform: ZoomTranform = { k: 1, x: 0, y: 0 };
 type ZoomTranform = { k: number, x: number, y: number };
@@ -29,6 +33,25 @@ export function initialize(canvas: HTMLCanvasElement) {
     console.log(positions);
     boothsProgramInfo = twgl.createProgramInfo(gl, [boothsVertexShaderSource, boothsFragmentSharedSource]);
     boothsBufferInfo = twgl.createBufferInfoFromArrays(gl, { a_position: { numComponents: 2, data: positions } });
+
+    textProgramInfo = twgl.createProgramInfo(gl, [textVertexShaderSource, textFragmentSharedSource]);
+    textBufferInfo = primitives.createPlaneBufferInfo(gl, 100, 200, 1, 1, m4.rotateX(Math.PI / 2));
+debugger
+    var textCanvas = makeTextCanvas("Hello!", 100, 26);
+    textWidth = textCanvas.width;
+    textHeight = textCanvas.height;
+    var textTex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, textTex);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textCanvas);
+    // make sure we can render it even if it's not a power of 2
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    textUniforms = {
+        u_matrix: m4.identity(),
+        u_texture: textTex,
+    };
 
     requireRedraw()
 }
@@ -48,7 +71,23 @@ function draw() {
     twgl.setBuffersAndAttributes(gl, boothsProgramInfo, boothsBufferInfo);
     twgl.setUniforms(boothsProgramInfo, { u_matrix: matrix });
 
-    gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
+    // gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
+
+      // use just the view position of the 'F' for the text
+      var textMatrix = m4.identity();// .ortho(0, gl.canvas.width, gl.canvas.height, 0, -1, 1);
+    // scale the F to the size we need it.
+    // textMatrix = m4.scale(textMatrix, textWidth, textHeight, 1);
+
+    // setup to draw the text.
+    gl.useProgram(textProgramInfo.program);
+
+    twgl.setBuffersAndAttributes(gl, textProgramInfo, textBufferInfo);
+
+    m4.copy(textMatrix, textUniforms.u_matrix);
+    twgl.setUniforms(textProgramInfo, textUniforms);
+
+    // Draw the text.
+    gl.drawElements(gl.TRIANGLES, textBufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
 }
 
 
@@ -72,8 +111,20 @@ function sizeCanvases() {
         canvas.width = cWidth;
         canvas.height = cHeight;
     }
-}
 
+}
+const textCtx = document.createElement("canvas").getContext("2d");
+function makeTextCanvas(text, width, height): HTMLCanvasElement {
+    textCtx.canvas.width = width;
+    textCtx.canvas.height = height;
+    textCtx.font = "20px monospace";
+    textCtx.textAlign = "center";
+    textCtx.textBaseline = "middle";
+    textCtx.fillStyle = "black";
+    textCtx.clearRect(0, 0, textCtx.canvas.width, textCtx.canvas.height);
+    textCtx.fillText(text, width / 2, height / 2);
+    return textCtx.canvas;
+}
 
 export function applyZoomTransform(transform: { k: number, x: number, y: number }) {
     zoomTranform = transform;
@@ -98,6 +149,27 @@ void main() {
 }`;
 
 
+const textVertexShaderSource = `attribute vec4 a_position;
+attribute vec2 a_texcoord;
+uniform mat4 u_matrix;
+varying vec2 v_texcoord;
+
+void main() {
+  // Multiply the position by the matrix.
+  gl_Position = u_matrix * a_position;
+
+  // Pass the texcoord to the fragment shader.
+  v_texcoord = a_texcoord;
+}`;
+
+const textFragmentSharedSource = `precision mediump float;
+// Passed in from the vertex shader.
+varying vec2 v_texcoord;
+uniform sampler2D u_texture;
+
+void main() {
+   gl_FragColor = vec4(1, 0, 0.5, 1);//texture2D(u_texture, v_texcoord);// + vec4(1,0,0,1);
+}`;
 //////////////////////////////////////////////////////////////////
 // Data
 const booths = store.getters.boothsArray as Booth[];
