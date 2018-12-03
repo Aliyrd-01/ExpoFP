fd77c790141f0fa8086b2811747d56ad7470eb6b
Evgeny Tukin <evgeny.tukin@9rays.net>
2018-12-03 13:35:32 +0300

    -

--- src/components/Map/draw.ts
+++ src/components/Map/draw.ts
@@ -1,19 +1,12 @@
 import { svgWidth, svgHeight } from '@/tools/svg'
-import * as twgl from 'twgl.js'
+// import * as twgl from 'twgl.js'
 import { m4 } from 'twgl.js'
 import { drawBooths } from './draw-booths'
-// import { getFpDestinationRectangle } from './utils';
-// import c from './drawing-context'
-// import { drawFpGrid, drawDebug } from './draw-debug';
 import settings from '@/settings';
-// import { createShader, createProgram } from './utils';
-// import { drawSprites } from './draw-sprites';
-// export { getBoothIdFromClientXy } from './booth-by-xy';
 
 let canvas: HTMLCanvasElement;
 let gl: WebGLRenderingContext;
-// let programInfo: any;
-// let matrixUniformLocation: WebGLUniformLocation;
+
 let zoomTranform: ZoomTranform = { k: 1, x: 0, y: 0 };
 
 type ZoomTranform = { k: number, x: number, y: number };
@@ -23,35 +16,6 @@ export function applyZoomTransform(transform: { k: number, x: number, y: number 
     requireRedraw();
 }
 
-
-// initialized:
-// canvas
-// gl
-
-const width = svgWidth;
-const height = svgHeight;
-const booths = store.getters.boothsArray as Booth[];
-
-const positions = [];
-
-function addRect(x1, x2, y1, y2) {
-    positions.push(x1, y1);
-    positions.push(x2, y1);
-    positions.push(x1, y2);
-
-    positions.push(x2, y1);
-    positions.push(x1, y2);
-    positions.push(x2, y2);
-}
-
-// addRect(0, width / 3, 0, height / 3);
-// addRect(width / 2, height / 2, width, height);
-
-for (const b of booths) {
-    addRect(b.rect.x1, b.rect.x2, b.rect.y1, b.rect.y2);
-}
-
-console.log(positions);
 
 let animatedFrame: number;
 export function requireRedraw() {
@@ -71,32 +35,13 @@ function draw() {
     // matrix = m4.scale(matrix, [devicePixelRatio, devicePixelRatio, 1]);
 
 
-    const scale = Math.min(gl.canvas.width / width, gl.canvas.height / height) * 0.95;
+    const scale = Math.min(gl.canvas.width / svgWidth, gl.canvas.height / svgHeight) * 0.95;
     // const scale = 1.7;
 
     matrix = m4.scale(matrix, [scale, scale, 1]);
-    matrix = m4.translate(matrix, [-width / 2, -height / 2, 0]);
+    matrix = m4.translate(matrix, [-svgWidth / 2, -svgHeight / 2, 0]);
 
-    // apply the d3 translate and zoom
-
-
-
-    // // translate the unit quad to the center 
-    // matrix = m4.translate(matrix, [width / 2, height / 2, 0]);
-    // // make the unit quad be half the size of the canvas
-    // matrix = m4.scale(matrix, [width / 2, height / 2 , 1]);
-
-    // gl.uniformMatrix4fv(matrixUniformLocation, false, matrix);
-    // gl.clear(gl.COLOR_BUFFER_BIT);
-    // gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
-
-    // const uniforms = {
-    //     u_matrix: matrix,
-    // };
-
-    // gl.useProgram(programInfo.program);
-// 
-    // twgl.setUniforms(programInfo, uniforms);
+  
     gl.clear(gl.COLOR_BUFFER_BIT);
     drawBooths(gl, matrix);
     // gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
@@ -107,15 +52,6 @@ export function initialize(canvasParam: HTMLCanvasElement) {
     canvas = canvasParam;
     sizeCanvases();
     gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-
-
-    // programInfo = twgl.createProgramInfo(gl, [vertexShaderSource, fragmentSharedSource]);
-
-    // const arrays = { a_position: { numComponents: 2, data: positions } };
-    // const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
-
-    // gl.useProgram(programInfo.program);
-    // twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
 
     draw();
 }
