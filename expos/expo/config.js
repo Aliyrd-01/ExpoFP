18f600497262cff05b5a58e4dbc28704ab8e446b
Evgeny Tukin <evgeny.tukin@9rays.net>
2018-12-07 11:33:08 +0300

    -

--- expos/expo/config.js
+++ expos/expo/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     // required
-    title: 'Expo! Expo! 2018',
+    title: 'ExpoFP',
     homeUrl : 'https://expofp.com/',
     logoUrl: 'logo.svg',
     gTag: 'UA-857963-22',
     // optional overrides
-    dataUrl: `https://s3.amazonaws.com/efp-data/expos/expo2018/data`
+    dataUrl: `https://s3.amazonaws.com/efp-data/expos/expo/data`
 }
