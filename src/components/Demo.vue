1ebe1e5d7076272fd8b5820857fe9070fa72624e
Evgeny Tukin <evgeny.tukin@9rays.net>
2018-12-10 20:16:24 +0300

    -

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="demo" :class='{hidden}' v-if="demo">
+    <div class="demo" :class='{hidden, top}' v-if="demo">
         <section>
             <a href='' @click.prevent='dismiss()' class='dismiss'>Dismiss</a>
-            This is not an official Expo!Expo!® plan. Get your free expo floor plan at <a href='https://expofp.com/'>ExpoFP.com</a>
+            <span>This is not an official Expo!Expo!® plan. </span>
+            <span>Get your free expo floor plan at <a href='https://expofp.com/'>ExpoFP.com</a></span>
 
         </section>
     </div>
@@ -10,23 +11,26 @@
 
 <script lang="ts">
 export default {
-    data: () => ({ hidden: true , key: 'note-dismissed' }),
+    data: () => ({ hidden: true , key: 'note-dismissed1' }),
     computed: {
         demo() {
             return EFP_EXPO === "expo";
+        },
+        top(){
+            return window.innerWidth <= 820;
         }
     },
     mounted() {
         if (this.demo && !sessionStorage.getItem(this.key)){
-            if (window.innerWidth > 820)        
-            {
-                window.setTimeout(() => {
-                    this.hidden = false;
-                }, 2000);
-            } else {
-                alert(`This is not an official Expo!Expo!® plan. Get your free expo floor plan at ExpoFP.com`);
-                this.dismiss();
-            }
+            // if (window.innerWidth > 820)        
+            // {
+            window.setTimeout(() => {
+                this.hidden = false;
+            }, 2000);
+            // } else {
+            //     alert(`This is not an official Expo!Expo!® plan. Get your free expo floor plan at ExpoFP.com`);
+            //     this.dismiss();
+            // }
         }
     },
     methods: {
@@ -47,6 +51,7 @@ export default {
     background: linear-gradient(90deg, #e5175c, #5c17e5);
     /* top: 0; */
     color: #fff;
+   
 
     > section {
         font-size: 0.8rem;
@@ -75,8 +80,25 @@ export default {
     transition: all 0.5s;
     opacity: 1;
     bottom: 0;
+     &.top {
+        bottom: unset;
+        top: 0;
+        span {
+            display: inline-block;
+        }
+        .dismiss{
+            height: 100%;
+            display: flex;
+            align-items: center;
+            padding: 0 0.8rem;
+        }
+    }
+
     &.hidden {
         transform: translate(0, 100%);
+    }
+     &.top.hidden {
+        transform: translate(0, -100%);
     }
 }
 </style>
