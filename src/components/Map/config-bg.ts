ec99912a2c6e54317eb8dafd7c30855ce22fbb6d
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-01-18 12:09:49 +0300

    -

--- src/components/Map/config-bg.ts
+++ src/components/Map/config-bg.ts
@@ -8,10 +8,9 @@ import svg from '@/tools/svg'
 export default function configBg() {
     const drawer = requireDrawer('bg', TriangleDrawer);
 
-    const color1 = [0, 0, 0, 0.5] as Vec4;
+    // const color1 = [0, 0, 0, 0.5] as Vec4;
     const paths = (d3.select(svg).select('#BG').selectAll('path').nodes() as SVGPathElement[]);
 
-    // TODO: move meshing to build stage
     for (const p of paths) {
         const d = parseInt(p.getAttribute('data-index'));
         const color = ColorInfo.fromHex(Color(p.style.fill).hex()).toVec4();
@@ -41,11 +40,7 @@ export default function configBg() {
     for (const ro of rects) {
         const r = Rect.fromSvgRectElement(ro);
         const color = ColorInfo.fromHex(Color(ro.style.fill).hex()).toVec4();
-        // const c = 0.5;
-        // color[0] = color[0] * c;
-        // color[1] = color[1] * c;
-        // color[2] = color[2] * c;
-        // color[3] = c;
+
 
         drawer.addObject({
             p0: [r.x1, r.y1],
