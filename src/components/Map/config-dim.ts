863737de2f0c8cac4a370c9c1aae6c5805284d5e
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-01-19 23:31:21 +0300

    Animate DIM

--- src/components/Map/config-dim.ts
+++ src/components/Map/config-dim.ts
@@ -2,7 +2,7 @@ import { allDrawers, requireUpdate } from "./draw";
 import animate from './animate';
 
 
-// let dim = 0;
+let dim = 0;
 
 export default function configDim() {
     requireUpdate(update);
@@ -13,9 +13,26 @@ export default function configDim() {
     });
 }
 
+let cancelAnimation: () => void;
+
 function update() {
+    const targetDim = store.getters.dimmed ? 1 : 0;
+    if (targetDim === dim) return;
+    if (cancelAnimation) cancelAnimation();
+    if (targetDim == 1) {
+        cancelAnimation = animate(0, 200, null, d3.interpolateNumber(0, targetDim), (v) => {
+            dim = v;
+            setAllDrawers();
+        })
+    } else {
+        dim = targetDim;
+        setAllDrawers();
+    }
+}
+
+function setAllDrawers() {
     for (const d of allDrawers) {
-        d.dim = store.getters.dimmed ? 1 : 0;
+        d.dim = dim;
     }
 }
 
