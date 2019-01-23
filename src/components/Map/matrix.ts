7b52a2364b62c7ce04161594dcd070b6474e14fd
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-01-23 14:58:35 +0300

    WIP

--- src/components/Map/matrix.ts
+++ src/components/Map/matrix.ts
@@ -11,8 +11,9 @@ let ptscale: number;
 // svg -> canvas point (when unzoomed) 
 let pxSvgScale: number;
 // replace above with:
-// svg -> browser point matrix (unzoomed)
+// svg -> browser px matrix (unzoomed)
 let svgPxUnzoomedMatrix: number[][];
+let visibleRectPt: Rect;
 
 //
 // dependencies and misc
@@ -25,7 +26,6 @@ let canvasHeight: number;
 let visibleRect: Rect;
 let visibleScale: number;
 let zoomTransform: ZoomTransform;
-
 
 //
 // setters
@@ -54,7 +54,6 @@ export function setVisbleScale(scale: number) {
 //
 // getters
 //
-
 export function getMatrix() { ensureAll(); return matrix; }
 export function getPtscale() { ensureAll(); return ptscale; }
 export function getPxSvgMatrix() { ensureAll(); return pxSvgMatrix; }
@@ -65,40 +64,51 @@ export function getVisibleRect() { return visibleRect; }
 //
 // subscribe
 //
-const ptscaleChangeSubscribers: ((ptscale:number) => void)[] = [];
+const ptscaleChangeSubscribers: ((ptscale: number) => void)[] = [];
 export function subscribePtscaleChange(cb: (ptscale) => void) { ptscaleChangeSubscribers.push(cb); }
 function firePtscaleChange() { ptscaleChangeSubscribers.forEach(x => x(ptscale)); }
 
-const matrixChangeSubscribers: ((matrix:number[][]) => void)[] = [];
-export function subscribeMatrixChange(cb: (ptscale:number[][]) => void) { matrixChangeSubscribers.push(cb); }
+const matrixChangeSubscribers: ((matrix: number[][]) => void)[] = [];
+export function subscribeMatrixChange(cb: (ptscale: number[][]) => void) { matrixChangeSubscribers.push(cb); }
 function fireMatrixChange() { matrixChangeSubscribers.forEach(x => x(matrix)); }
-
 
 //
 // core
 //
+
+// function applyTransform(m4method, m4param, matrices) {
+//     matrices.forEach(m => m4method(m, m4param, m));
+// }
+
 function ensureAll() {
     if (!dirty) return;
     dirty = false;
 
-    const vRect = visibleRect.scale(devicePixelRatio);
+    visibleRectPt = visibleRect.scale(devicePixelRatio);
 
     matrix = m4.ortho(0, canvasWidth, canvasHeight, 0, -1, 1);
     pxSvgMatrix = m4.scale(m4.identity(), [1 / devicePixelRatio, 1 / devicePixelRatio, 1]);
     // px/svg scale
-    pxSvgScale = Math.min(vRect.w / svgWidth, vRect.h / svgHeight) * visibleScale;
+    pxSvgScale = Math.min(visibleRectPt.w / svgWidth, visibleRectPt.h / svgHeight) * visibleScale;
 
-    const matrices = [matrix, pxSvgMatrix];
-    const actions = [
-        [m4.translate, [zoomTransform.x * devicePixelRatio, zoomTransform.y * devicePixelRatio, 0]],
-        [m4.scale, [zoomTransform.k, zoomTransform.k, 1]],
-        [m4.translate, [vRect.cx, vRect.cy, 0]],
-        [m4.scale, [pxSvgScale, pxSvgScale, 1]],
-        [m4.translate, [-svgWidth / 2, -svgHeight / 2, 0]],
-    ];
+    const zoomMatrix = m4.translation([zoomTransform.x * devicePixelRatio, zoomTransform.y * devicePixelRatio, 0]);
+    m4.scale([zoomTransform.k, zoomTransform.k, 1], zoomMatrix);
 
-    // do the magic
-    actions.forEach(a => matrices.forEach(m => a[0](m, a[1], m)));
+    const centerSvgMatrix = m4.translation([visibleRectPt.cx, visibleRectPt.cy, 0]);
+    m4.scale(centerSvgMatrix, [pxSvgScale, pxSvgScale, 1], centerSvgMatrix);
+    m4.translate(centerSvgMatrix, [-svgWidth / 2, -svgHeight / 2, 0], centerSvgMatrix);
+
+    // applyTransform(m4.translate, [zoomTransform.x * devicePixelRatio, zoomTransform.y * devicePixelRatio, 0], [matrix, pxSvgMatrix]);
+    // applyTransform(m4.scale, [zoomTransform.k, zoomTransform.k, 1], [matrix, pxSvgMatrix]);
+    m4.multiply(matrix, zoomMatrix, matrix);
+    m4.multiply(pxSvgMatrix, zoomMatrix, pxSvgMatrix);
+
+    m4.multiply(matrix, centerSvgMatrix, matrix);
+    m4.multiply(pxSvgMatrix, centerSvgMatrix, pxSvgMatrix);
+
+    // applyTransform(m4.translate, [visibleRectPt.cx, visibleRectPt.cy, 0], [matrix, pxSvgMatrix]);
+    // applyTransform(m4.scale, [pxSvgScale, pxSvgScale, 1], [matrix, pxSvgMatrix]);
+    // applyTransform(m4.translate, [-svgWidth / 2, -svgHeight / 2, 0], [matrix, pxSvgMatrix]);
 
     m4.inverse(pxSvgMatrix, pxSvgMatrix);
 
