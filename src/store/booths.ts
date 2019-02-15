2690df67c0274ded749015a624832240503d6b9c
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-02-15 21:01:50 +0300

    -

--- src/store/booths.ts
+++ src/store/booths.ts
@@ -1,6 +1,6 @@
-import svg from '@/tools/svg'
-import { generateUniqueSlug } from '@/services/slug';
-import { getNextId } from '@/services/id';
+import svg from "@/tools/svg";
+import { generateUniqueSlug } from "@/services/slug";
+import { getNextId } from "@/services/id";
 // import {replaceLetter} from '@/tools/demo-replace'
 
 //__data.booths.splice(3);
@@ -8,7 +8,6 @@ import { getNextId } from '@/services/id';
 const booths = __data.booths.reduce((a, c) => (a[c.id] = c) && a, {} as { [id: number]: Booth });
 //const boothsBySlug = new Map<string, Booth>();
 const boothsByName = new Map<string, Booth>();
-
 
 // setup slugs
 for (const b of Object.values(booths)) {
@@ -23,25 +22,39 @@ for (const b of Object.values(booths)) {
     boothsByName.set(b.name.toLowerCase(), b);
 }
 
-
-for (const r of d3.select(svg).select('#Booths').selectAll('rect').nodes() as SVGRectElement[]) {
+for (const r of d3
+    .select(svg)
+    .select("#Booths")
+    .selectAll("rect")
+    .nodes() as SVGRectElement[]) {
     const idInSvg = (r.getAttribute("data-name") || r.id).substring(1).toLowerCase();
     let booth = boothsByName.get(idInSvg);
     if (!booth) {
         console.error("SVG booth not found in __data: ", idInSvg);
         // create fake booth
-        booth = { id: getNextId(), name: idInSvg.toUpperCase(), slug: generateUniqueSlug(idInSvg), exhibitors: [], error: true } as any;
+        booth = {
+            id: getNextId(),
+            name: idInSvg.toUpperCase(),
+            slug: generateUniqueSlug(idInSvg),
+            exhibitors: [],
+            error: true
+        } as any;
         booths[booth.id] = booth;
     } //else
 
     const transform = r.getAttribute("transform");
-    if (transform){
-        const m = transform.match(/translate\(([\-0-9\.]+) ([\-0-9\.]+)\) rotate\(([\-0-9\.]+)\)/);
-        if (m){
-            const translateX = parseFloat(m[1]);
-            const translateY = parseFloat(m[2]);
-            const rotate = parseFloat(m[3]);
-            booth.rotate = -rotate * Math.PI / 180;
+    if (transform) {
+        const mt = transform.match(/translate\(([\-0-9\.]+) ([\-0-9\.]+)\) rotate\(([\-0-9\.]+)\)/);
+        if (mt) {
+            const translateX = parseFloat(mt[1]);
+            const translateY = parseFloat(mt[2]);
+            const rotate = parseFloat(mt[3]);
+            booth.rotate = (-rotate * Math.PI) / 180;
+        } else {
+            const mm = transform.match(/matrix\(\s*([\-0-9\.]+)\s*,\s*([\-0-9\.]+)\s*,\s*([\-0-9\.]+)\s*,\s*([\-0-9\.]+)\s*,\s*([\-0-9\.]+)\s*,\s*([\-0-9\.]+)\s*\)/);
+            if (mm) {
+                booth.rotate = -parseFloat(mm[2]);
+            }
         }
     }
 
@@ -51,13 +64,13 @@ for (const r of d3.select(svg).select('#Booths').selectAll('rect').nodes() as SV
 for (const b of Object.values(booths)) {
     if (!b.rect) {
         console.error("__data booth not found in SVG:", b.name, b);
-        delete booths[b.id]
+        delete booths[b.id];
     }
 }
 
 export default {
     state: booths,
     getters: {
-        boothsArray: (state: any) => Object.values(state),
+        boothsArray: (state: any) => Object.values(state)
     }
-}
+};
