b76498544b2ba0a9eb5d7346d7fb04564abdf881
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-02-16 14:15:34 +0300

    -

--- src/components/Ws.vue
+++ src/components/Ws.vue
@@ -1,19 +1,23 @@
 <template>
     <section class="ws" :style="style">
-        <a :href="`?${e.slug}`" v-for="e in exhibitors" :key="e.slug" class="ws__exhibitor" :style='{height: `${wsHeightPx}px`}'
+        <a :href="`?${e.slug}`" v-for="e in adv" :key="e.slug" class="ws__exhibitor" 
+        :style='{height: `${wsHeightPx}px`}'
             @click.prevent="select(e.id)"><img :src='e.logo' :alt='e.name'></a>
     </section>
 </template>
 
 <script lang="ts">
 import { mapGetters, mapState } from "vuex";
+import { shuffle } from "@/utils";
+import { remsToPixels } from '@/components/Map/utils';
 
 
 export default {
-    data: () => ({}),
+    data: () => ({ groups: [], groupIndex: 0 }),
     computed: {
         ...mapState([
             "overlayWidthRems",
+            "screenSize"
         ]),
         ...mapGetters([
             "overlayPosition",
@@ -23,17 +27,58 @@ export default {
         height() {
 
         },
+        adv() {
+            const i = this.groupIndex % this.groups.length;
+            return this.groups[i];
+        },
         style() {
             const width = this.overlayPosition === "left" ? `calc(100% - ${this.$store.state.overlayWidthRems}rem)` : '100%';
+            // const display = this.adv.length ? 'flex' : 'none';
             // const height = `${this.wsHeightPx}px`;
             return { width };
         },
-        exhibitors() {
-            const adv = this.exhibitorsArray.filter(x => x.advertise);
-            return adv;
-        }
+    },
+    mounted() {
+        this.$watch("screenSize", this.calcGroups);
+        this.$watch("groups", this.setupNextGroup);
+        this.calcGroups();
+        window.setInterval(this.setupNextGroup, 1000);
     },
     methods: {
+        setupNextGroup() {
+            this.groupIndex++;
+        },
+        calcGroups() {
+            // split into groups
+            const all: Exhibitor[] = shuffle(this.exhibitorsArray.filter(x => x.advertise));
+            const groups: Exhibitor[][] = [];
+            let currentGroup: Exhibitor[];
+            let currentGroupWidth: number;
+            const rectWidth = (this.$el as HTMLDivElement).getBoundingClientRect().width;
+            const maxWidth = rectWidth - remsToPixels(0.3) * 2; // exclude padding
+            const minSpaceBetween = remsToPixels(0.5);
+            let loaded = 0;
+            const loadingImages = [];
+
+            all.forEach(x => {
+                const img = new Image();
+                loadingImages.push(img);
+                img.onload = () => {
+                    const width = img.width * this.wsHeightPx / img.height;
+                    loaded++;
+                    if (!currentGroup || currentGroup.length > 0 && currentGroupWidth + width > maxWidth) {
+                        currentGroup = [];
+                        currentGroupWidth = 0;
+                        groups.push(currentGroup);
+                    }
+                    currentGroupWidth += width;
+                    currentGroup.push(x);
+                    // console.log('currentGroupWidth', width, currentGroupWidth, maxWidth)
+                    if (loaded === all.length) this.groups = groups;
+                };
+                img.src = x.logo;
+            })
+        },
         select(id) {
             this.$store.dispatch("clickExhibitor", id);
         }
@@ -49,7 +94,7 @@ export default {
     height: 0;
     /* background: #999; */
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     padding: 0 0.3rem;
 
     &__exhibitor {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -26,6 +26,26 @@ export function debounce(func: Function, wait: number, immediate: boolean) {
     };
 }
 
+export function shuffle<T>(array: T[]) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+
 export function rtp(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
