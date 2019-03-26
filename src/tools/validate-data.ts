469e9e8d8b1ffa49d57bf61e1e11680b05f277db
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-03-26 12:44:17 +0300

    -

--- src/tools/validate-data.ts
+++ src/tools/validate-data.ts
@@ -27,6 +27,9 @@ if (__settings.debug) {
         errors.forEach(e => console.warn(e));
     }
 }
+if (!__data.exhibitors) __data.exhibitors = [];
+if (!__data.booths) __data.booths = [];
+if (!__data.categories) __data.categories = [];
 
 // some data fixes (expo-specific will be removed)
 if (!__data.homeUrl && EFP_EXPO === "jtrade19") __data.homeUrl = "https://www.jtrade.co.uk/";
