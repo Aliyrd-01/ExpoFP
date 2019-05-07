c51f23656498bca5d2b75324a1e56f8809f04813
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-05-07 22:37:15 +0300

    -

--- src/components/Map/config-booth-bookmark.ts
+++ src/components/Map/config-booth-bookmark.ts
@@ -5,8 +5,9 @@ import { subscribePtscaleChange, getPtscale } from './matrix';
 import { requireUpdate } from './draw';
 import Drawer from './Drawer';
 
+const bookmarkCanvasXL = createBookmarkCanvas(11);
 const bookmarkCanvasL = createBookmarkCanvas(8);
-const bookmarkCanvasM = createBookmarkCanvas(5);
+const bookmarkCanvasM = createBookmarkCanvas(6);
 
 
 export default function configBoothBookmark(booth: Booth) {
@@ -18,6 +19,18 @@ class BoothBookmarkDrawer extends BoothDrawerBase<Drawer> {
     constructor(booth: Booth) {
         super(booth, 'booth-bookmark', Drawer, 140);
         const r = this.booth.rect.withPadding(__fpBorderWidth / 2);
+
+        this.drawer.addObject({
+            id: this.getId("XL"),
+            rotateRadians: booth.rotate,
+            center: [r.cx, r.cy],
+            deltas: [-r.w / 2, -r.h / 2, r.w / 2, r.h / 2],
+            deltaPts: [0, -bookmarkCanvasXL.lineWidth - bookmarkCanvasXL.padding, -bookmarkCanvasXL.lineWidth - bookmarkCanvasXL.padding, 0],
+            scalePts: devicePixelRatio,
+            canvasTmp: bookmarkCanvasXL.canvas,
+            texPosition: 'righttop',
+            visible: false
+        });
 
         this.drawer.addObject({
             id: this.getId("L"),
@@ -70,7 +83,9 @@ class BoothBookmarkDrawer extends BoothDrawerBase<Drawer> {
         if (bookmarked) {
             const widthPx = this.booth.rect.w / ptscale / devicePixelRatio;
             const heightPx = this.booth.rect.h / ptscale / devicePixelRatio;
-            if (widthPx > 25 && heightPx > 25) {
+            if (widthPx > 50 && heightPx > 50) {
+                view = "XL";
+            } else if (widthPx > 25 && heightPx > 25) {
                 view = "L";
             } else if (widthPx > 14) {
                 view = "M";
@@ -80,7 +95,7 @@ class BoothBookmarkDrawer extends BoothDrawerBase<Drawer> {
         }
 
         const skipDimm = this.getBoothState().skipDim;
-        ["L", "M", "S"].forEach(x => {
+        ["XL", "L", "M", "S"].forEach(x => {
             this.drawer.updateVisible(this.getId(x), x === view);
             this.drawer.updateSkipdim(this.getId(x), skipDimm);
         });
