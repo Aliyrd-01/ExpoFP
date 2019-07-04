e85d68a03a87e1f834c6aae3f2e15decbabb6f12
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-07-04 11:03:50 +0300

    -

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -17,7 +17,7 @@
                 <button @click.stop.prevent=' shareBookmarks' v-if='bookmarkedArray.length' class="fas fa-share-square"
                     title="Share bookmarks"></button>
             </a>
-             <a href='javascript:print()' class="menu__item -print"><i class="fas fa-print"></i> Print</a>
+             <!-- <a href='javascript:print()' class="menu__item -print"><i class="fas fa-print"></i> Print</a> -->
             <!-- <a href='?seminars' @click.prevent='$store.dispatch("clickSeminars");' class="menu__item"><i class="fas fa-graduation-cap"></i> Seminars</a> -->
 
             <div class="menu__item" v-if="categoriesArray.length">Categories</div>
