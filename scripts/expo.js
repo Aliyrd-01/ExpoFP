16326c3c6cc58732965a60cc75212476b420c726
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-07-08 14:40:00 +0300

    Refactor deployment

--- scripts/expo.js
+++ scripts/expo.js
@@ -1,18 +1,26 @@
-const gitBranch = require('git-branch');
-//const defaultExpo = "jtrade19";
+const branch = require("git-branch").sync();
+const onMasterBranch = branch === "master";
+let expo = branch.startsWith("expo-") ? branch.replace(/^expo-/, "") : null;
+const onExpoBranch = !!expo;
 
-const branch = gitBranch.sync();
-const masterBranch = branch === "master";
-let expo = branch.replace(/^expo-/, '');
-const expoBranch = expo !== branch;
+const live = process.env.EFP_TARGET === "live";
 
 // let's have some expo branch for non-expo branch
-if (!expoBranch) {
-    expo = masterBranch ? "_template_for_new_event_" : "expo";
+if (!onExpoBranch) {
+    expo = onMasterBranch && live ? "_template_for_new_event_" : "expo";
 }
 
-if (!masterBranch && !expoBranch && process.env.EFP_TARGET === "live") {
-    throw new Error(`Unknown expo. Won't run LIVE on ${branch} branch - this is not expo or master branch.`);
-}
+// const masterDevExpo
+
+// if (!onMasterBranch && !onExpoBranch && live) {
+//     throw new Error(`Unknown expo. Won't run LIVE on ${branch} branch - this is not expo or master branch.`);
+// }
 
 module.exports = expo;
+
+module.exports = {
+    expo,
+    // branch,
+    onExpoBranch,
+    onMasterBranch
+};
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const fs = require('fs');
-const webpack = require('webpack');
-const expo = require('./scripts/expo');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const fs = require("fs");
+const webpack = require("webpack");
+const { expo } = require("./scripts/expo");
 let config;
 try {
     config = require(`./expos/${expo}/config`);
@@ -11,19 +11,19 @@ try {
 }
 
 const localDataExists = fs.existsSync(`./expos/${expo}/data`);
-const localDataUrl = (localDataExists && process.env.NODE_ENV === "development") ? './data' : null;
+const localDataUrl = localDataExists && process.env.NODE_ENV === "development" ? "./data" : null;
 
 const dataUrlBase = localDataUrl || config.dataUrl || `https://${expo}.expofp.com/data`;
 const dataUrlBaseDev = config.dataUrl || `https://s3.amazonaws.com/efp-data-dev/expos/${expo}/data`;
 const dataUrlBaseShow = config.dataUrl || `https://s3.amazonaws.com/efp-data-show/expos/${expo}/data`;
 
-console.info("Serving data from: ", dataUrlBase)
+console.info("Serving data from: ", dataUrlBase);
 // const gTag = config.gTag || 'UA-857963-22';
 //const logoUrl = config.logoUrl || `${expo}-logo.png`
 
 const define = {
     EFP_DATA_URL_BASE: JSON.stringify(dataUrlBase),
-    EFP_EXPO: JSON.stringify(expo),
+    EFP_EXPO: JSON.stringify(expo)
     //EFP_TITLE: JSON.stringify(config.title),
     //EFP_HOME_URL: JSON.stringify(config.homeUrl),
     //EFP_LOGO_URL: JSON.stringify(logoUrl),
@@ -31,64 +31,62 @@ const define = {
 };
 
 //const live = process.env.EFP_TARGET === "live";
-const dist = 'dist'; // + (live ? 'live' : 'dev');
+const dist = "dist"; // + (live ? 'live' : 'dev');
 
 function escapeRegExp(string) {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 }
 
 module.exports = {
     devServer: {
-        contentBase: [path.join(__dirname, 'public'), path.join(__dirname, `expos/${expo}`)]
+        contentBase: [path.join(__dirname, "public"), path.join(__dirname, `expos/${expo}`)]
     },
     outputDir: dist,
     publicPath: `./`,
     configureWebpack: {
         plugins: [
-            new CopyWebpackPlugin(
-                [{
+            new CopyWebpackPlugin([
+                {
                     from: path.join(__dirname, `expos/${expo}/{*.png,*.svg}`),
                     to: path.join(__dirname, dist),
                     context: path.join(__dirname, `expos/${expo}`)
-                }]
-            ),
+                }
+            ]),
             new webpack.DefinePlugin(define),
             {
-                apply: (compiler) => {
-                    compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                apply: compiler => {
+                    compiler.hooks.afterEmit.tap("AfterEmitPlugin", compilation => {
                         if (!fs.existsSync(path.join(__dirname, dist))) return;
-                        const prodIndex = path.join(__dirname, dist, 'index.html');
-                        const devIndex = path.join(__dirname, dist, 'index.dev.html');
-                        const showIndex = path.join(__dirname, dist, 'index.show.html');
-                        const data = fs.readFileSync(prodIndex, 'utf-8');
-                        fs.writeFileSync(devIndex, data.replace(new RegExp(escapeRegExp(dataUrlBase), 'g'), dataUrlBaseDev));
-                        fs.writeFileSync(showIndex, data.replace(new RegExp(escapeRegExp(dataUrlBase), 'g'), dataUrlBaseShow));
+                        const prodIndex = path.join(__dirname, dist, "index.html");
+                        const devIndex = path.join(__dirname, dist, "index.dev.html");
+                        const showIndex = path.join(__dirname, dist, "index.show.html");
+                        const data = fs.readFileSync(prodIndex, "utf-8");
+                        fs.writeFileSync(devIndex, data.replace(new RegExp(escapeRegExp(dataUrlBase), "g"), dataUrlBaseDev));
+                        fs.writeFileSync(showIndex, data.replace(new RegExp(escapeRegExp(dataUrlBase), "g"), dataUrlBaseShow));
                     });
                 }
             }
         ],
         resolve: {
             alias: {
-                'styles': path.resolve(__dirname, './src/styles/')
+                styles: path.resolve(__dirname, "./src/styles/")
             }
-        },
+        }
     },
     chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].minify = {
-                    collapseWhitespace: true,
-                    preserveLineBreaks: true,
-                    removeAttributeQuotes: true,
-                    removeComments: true,
-                    removeRedundantAttributes: true,
-                    removeScriptTypeAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    useShortDoctype: true
-                };
-                return args;
-            })
+        config.plugin("html").tap(args => {
+            args[0].minify = {
+                collapseWhitespace: true,
+                preserveLineBreaks: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            };
+            return args;
+        });
     },
     css: {
         loaderOptions: {
@@ -97,4 +95,4 @@ module.exports = {
             }
         }
     }
-}
+};
