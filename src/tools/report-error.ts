c5245807311dc05f3bfb9f8335b5e4393bcff714
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-07-22 17:26:49 +0300

    -

--- src/tools/report-error.ts
+++ src/tools/report-error.ts
@@ -6,21 +6,8 @@ export default function reportError(e: Partial<ErrorEvent>) {
     if (timeoutId) return;
 
     timeoutId = window.setTimeout(async function () {
-        const ipInfoRequest = await fetch('https://geo.ipify.org/api/v1?apiKey=at_3dMzE1vaZp2Kd8NxMV7HukiFFjutg');
-        const ipInfo = await ipInfoRequest.json();
 
-        __logger.log('ipify', ipInfo, ipInfoRequest);
-        let ipData;
-        if (ipInfoRequest.ok) {
-            ipData = {
-                ip: ipInfo.ip,
-                ...ipInfo.location
-            }
-        } else {
-            ipData = {
-                ip: ipInfo.messages
-            }
-        }
+        const ipData = await getIpData();
 
         const language = (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.language;
         const data = {
@@ -46,4 +33,26 @@ export default function reportError(e: Partial<ErrorEvent>) {
 
         __logger.log('Reporter response: ', await rawResponse.text());
     }, 2000);
+}
+
+async function getIpData() {
+    try {
+        const ipInfoRequest = await fetch('https://geo.ipify.org/api/v1?apiKey=at_3dMzE1vaZp2Kd8NxMV7HukiFFjutg');
+        const ipInfo = await ipInfoRequest.json();
+
+        __logger.log('ipify', ipInfo, ipInfoRequest);
+
+        if (ipInfoRequest.ok) {
+            return {
+                ip: ipInfo.ip,
+                ...ipInfo.location
+            }
+        } else {
+            return { ip: ipInfo.messages };
+        }
+    }
+    catch (e) {
+        __logger.error(e);
+        return { ip: e.message };
+    }
 }
