c2dd1ed363a512c873ab541ce3a68a0b6296eee9
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-08-15 17:12:55 +0300

    --wip-- [skip ci]

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Layout.scss";
 import Overlay from "./Overlay";
 import { observer } from "mobx-react-lite";
 import store from "../store";
+import logger from "../tools/logger";
+import { isWebGlSupported } from "../utils";
+import Map from "./Map/Map";
 
 export default observer(function Layout() {
     // const overlayPosition = "1";
@@ -12,6 +15,21 @@ export default observer(function Layout() {
     //         store.overlayPosition += 1;
     //     }
     // }));
+    const [fontsReady, setFontsReady] = useState(false);
+    useEffect(() => {
+        let set = false;
+        function doSet(cause) {
+            if (set) return;
+            set = true;
+            logger.log("fontsReady", cause);
+            setFontsReady(true);
+        }
+
+        window.setTimeout(doSet.bind(window, "timeout"), 5000);
+        window.addEventListener("load", doSet.bind(window, "load"));
+        const f = document["fonts"];
+        if (f && f.ready) f.ready.then(doSet.bind(window, "ready"));
+    }, []);
 
     return (
         <div className="layout">
@@ -21,9 +39,9 @@ export default observer(function Layout() {
                 <Ws />
                 <Controls />
                 <Areas />
-                <Overlay />
-                <Map v-if="fontsReady && webGlSupported" />
-                <Demo />
+                <Overlay /> */}
+                {fontsReady && isWebGlSupported && <Map />}
+                {/* <Demo />
                 <Debug />
                 <Pdf v-if="webGlSupported" /> */}
                 <div id="fps" />
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,22 @@
 import "./Map.scss";
-import { useObserver } from "mobx-react-lite";
+import { useObserver, useLocalStore } from "mobx-react-lite";
 import classNames from "classnames";
+import { useEffect, useRef, useMemo } from "react";
+// TODO: RESTORE - only use what's needed from d3
+import * as d3 from "d3";
 
 export default function Map() {
+    const el = useRef<HTMLCanvasElement>();
+    const $canvas = useMemo(() => d3.select(el.current), [el.current]);
+    const s = useLocalStore(() => ({
+        a: 1
+    }));
 
+    useEffect(() => {}, []);
 
-    
     return useObserver(() => (
         <canvas
+            ref={el}
             className={classNames({ map: true, moving: s.moving })}
             onMouseMove={handleMouseMove}
             onClick={handleClick}
