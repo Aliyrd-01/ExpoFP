0fbd52aecf9a8620958596d9ea9b53d6c1a142b9
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-08-26 18:13:51 +0300

    --wip-- [skip ci]

--- src/components/Map/drawing/config/config-booths.ts
+++ src/components/Map/drawing/config/config-booths.ts
@@ -1,50 +1,49 @@
-import configBoothBg from './config-booth-bg';
-import configBoothLabels from './config-booth-labels';
-import configBoothLabelsSpecial from './config-booth-labels-special';
-// import configBoothBookmark from './config-booth-bookmark';
-import configBoothBorder from './config-booth-border';
-// import { DrawerContext } from '../Drawer1';
-import RectPainter from '../painters/RectPainter';
-import animate from './animate';
-import settings from '../../../../tools/settings';
-import { boothStore } from '../../../../store';
-import { easeLinear } from 'd3-ease';
-import { interpolateNumber } from 'd3-interpolate';
-import { DrawerContext } from '../Drawer1';
+import { easeLinear } from "d3-ease";
+import { interpolateNumber } from "d3-interpolate";
+import { boothStore } from "../../../../store";
+import settings from "../../../../tools/settings";
+import { DrawerContext } from "../Drawer1";
+import RectPainter from "../painters/RectPainter";
+import animate from "./animate";
+import configBoothBg from "./config-booth-bg";
+import configBoothBookmark from "./config-booth-bookmark";
+import configBoothBorder from "./config-booth-border";
+import configBoothLabels from "./config-booth-labels";
+import configBoothLabelsSpecial from "./config-booth-labels-special";
 
 export default function configBooths(context: DrawerContext) {
     const booths = boothStore.booths;
-    const configFuncs = [configBoothBg, configBoothLabels, configBoothLabelsSpecial, /*configBoothBookmark,*/ configBoothBorder] as
-        ((DrawerContext, Booth) => void | { unlock: () => void })[];//configBoothType,
+    const configFuncs = [configBoothBg, configBoothLabels, configBoothLabelsSpecial, configBoothBookmark, configBoothBorder] as ((
+        DrawerContext,
+        Booth
+    ) => void | { unlock: () => void })[]; //configBoothType,
 
     // const after = [];
     const lockedDrawers: { unlock: () => void }[] = [];
     for (const func of configFuncs) {
-        //const drawer = 
+        //const drawer =
         const name = "config-func " + func.name;
         if (settings.debug) console.time(name);
         for (const b of booths) {
-            // const afterFunc = 
+            // const afterFunc =
             const dr = func(context, b);
-            if (dr) lockedDrawers.push(dr)
+            if (dr) lockedDrawers.push(dr);
             // if (afterFunc) after.push(afterFunc);
         }
         if (settings.debug) console.timeEnd(name);
         // if (drawer) ar.push(drawer);
     }
 
-    const labelsPainter = context.requirePainter('booth-label') as RectPainter;
+    const labelsPainter = context.requirePainter("booth-label") as RectPainter;
     if (context.updatable) {
         labelsPainter.alpha = 0;
-    } 
+    }
 
-    return function () {
+    return function() {
         for (const dr of lockedDrawers) {
             dr.unlock();
         }
-        animate(0, 300, easeLinear, interpolateNumber(0, 1),
-            context.requireUpdate.bind(context),
-            v => (labelsPainter.alpha = v));
+        animate(0, 300, easeLinear, interpolateNumber(0, 1), context.requireUpdate.bind(context), v => (labelsPainter.alpha = v));
     };
     // if (after.length) return function () { for (const f of after) { f(); } }
 }
