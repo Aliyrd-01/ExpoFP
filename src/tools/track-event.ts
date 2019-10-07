fc19c59aeef954c3d237a495cec718a5e0c3c4bd
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-10-07 18:26:49 +0300

    --wip-- [skip ci]

--- src/tools/track-event.ts
+++ src/tools/track-event.ts
@@ -7,6 +7,6 @@ export default function trackEvent(type: "load", value?: any) {
     let url = data.trackerUrl;
     url += url.indexOf("?") === -1 ? "?" : "&";
     url += "type=" + encodeURIComponent(type);
-    if (value !== undefined) url += "value=" + encodeURIComponent(value);
+    if (value !== undefined) url += "&value=" + encodeURIComponent(value);
     fetch(url, {cache: "no-store"});
 }
