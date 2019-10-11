bad6216e4ac4dbb04d6be40a524b1b59a2c9d66a
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-10-11 16:13:20 +0300

    --wip-- [skip ci]

---
+++ public/exhibitor-list.js
@@ -0,0 +1,30 @@
+(function(d) {
+    var mm = d.getElementsByTagName("script");
+    var expo;
+    var m;
+    for (var i = 0; i < mm.length; i++) {
+        var m = mm[i];
+        var src = m.getAttribute("data-src") || m.src;
+        var ma = src.match(/^http(s?):\/\/([^.]+)\.expofp\.com/);
+        console.log(src);
+        if (ma) {
+            expo = ma[2];
+            break;
+        }
+    }
+    if (!expo) {
+        console.error("Cannot find expo");
+    }
+
+    var s = d.createElement("script");
+    s.src = "https://expofp.com/js_dist/embed/exhibitors-list.min.js";
+    s.async = 1;
+
+    console.log("Loading exhibitors list:", expo, m);
+
+    s.onload = function() {
+        exhibitorsList("#exhibitors-list", expo);
+    };
+
+    m.parentNode.insertBefore(s, m);
+})(document);
