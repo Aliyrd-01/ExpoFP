1f7737c1a4e65fa905b3f91d9a3a709b16ee6545
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-10-29 15:46:54 +0300

    --wip-- [skip ci]

--- src/components/Booth.tsx
+++ src/components/Booth.tsx
@@ -37,93 +37,92 @@ function Booth() {
         }
     }));
 
-    //return useObserver(() => {
-    const bar = <div className="booth__bar">{s.title}</div>;
-    let content: JSX.Element = null;
-    if (s.regular) {
-        const b = s.regular;
+    return useObserver(() => {
+        const bar = <div className="booth__bar">{s.title}</div>;
+        let content: JSX.Element = null;
+        if (s.regular) {
+            const b = s.regular;
 
-        const exhibitors = b.exhibitors.map(x => <ExhibitorRow key={x.id} exhibitor={x} className="list-row" />);
+            const exhibitors = b.exhibitors.map(x => <ExhibitorRow key={x.id} exhibitor={x} className="list-row" />);
 
-        if (data.hideCompanies && b.reserved) {
-            content = (
-                <div className="booth__content -reg">
-                    <div>Reserved</div>
-                </div>
-            );
-        } else if (b.exhibitors.length === 0) {
-            content = (
-                <>
+            if (data.hideCompanies && b.reserved) {
+                content = (
                     <div className="booth__content -reg">
-                        {b.onHold && <div>On Hold</div>}
+                        <div>Reserved</div>
+                    </div>
+                );
+            } else if (b.exhibitors.length === 0) {
+                content = (
+                    <>
+                        <div className="booth__content -reg">
+                            {b.onHold && <div>On Hold</div>}
 
-                        <div className="booth__infos">
-                            {b.type && !b.onHold && (
-                                <div className="booth__info">
-                                    <i className="fas fa-cube" />
-                                    <div className="booth__info-title">{data.boothTerm} Type</div>
-                                    <div className="booth__info-val">{b.type}</div>
+                            <div className="booth__infos">
+                                {b.type && !b.onHold && (
+                                    <div className="booth__info">
+                                        <i className="fas fa-cube" />
+                                        <div className="booth__info-title">{data.boothTerm} Type</div>
+                                        <div className="booth__info-val">{b.type}</div>
+                                    </div>
+                                )}
+                                {b.size && !b.onHold && (
+                                    <div className="booth__info">
+                                        <i className="fas fa-expand-alt" />
+                                        <div className="booth__info-title">Size</div>
+                                        <div className="booth__info-val">{b.size}</div>
+                                    </div>
+                                )}
+                                {b.price && !b.onHold && b.price !== "0" && (
+                                    <div className="booth__info">
+                                        <i className="fas fa-tag" />
+                                        <div className="booth__info-title">Price</div>
+                                        <div className="booth__info-val">{b.price}</div>
+                                    </div>
+                                )}
+                            </div>
+                            {data.reserveInstructions && !b.onHold && (
+                                <span
+                                    dangerouslySetInnerHTML={{ __html: data.reserveInstructions }}
+                                    className="booth__reserve-instructions"
+                                />
+                            )}
+
+                            {s.showBuy && (
+                                <div className="booth__buy">
+                                    <a href={b.buyUrl} rel="noopener">
+                                        Buy
+                                    </a>
                                 </div>
                             )}
-                            {b.size && !b.onHold && (
-                                <div className="booth__info">
-                                    <i className="fas fa-expand-alt" />
-                                    <div className="booth__info-title">Size</div>
-                                    <div className="booth__info-val">{b.size}</div>
-                                </div>
-                            )}
-                            {b.price && !b.onHold && b.price !== "0" && (
-                                <div className="booth__info">
-                                    <i className="fas fa-tag" />
-                                    <div className="booth__info-title">Price</div>
-                                    <div className="booth__info-val">{b.price}</div>
+                            {s.showReserve && (
+                                <div className="booth__buy">
+                                    <a href={b.reserveUrl || b.buyUrl} rel="noopener">
+                                        {s.reserveTitle}
+                                    </a>
                                 </div>
                             )}
                         </div>
-                        {data.reserveInstructions && !b.onHold && (
-                            <span
-                                dangerouslySetInnerHTML={{ __html: data.reserveInstructions }}
-                                className="booth__reserve-instructions"
-                            />
-                        )}
-
-                        {s.showBuy && (
-                            <div className="booth__buy">
-                                <a href={b.buyUrl} rel="noopener">
-                                    Buy
-                                </a>
-                            </div>
-                        )}
-                        {s.showReserve && (
-                            <div className="booth__buy">
-                                <a href={b.reserveUrl || b.buyUrl} rel="noopener">
-                                    {s.reserveTitle}
-                                </a>
-                            </div>
-                        )}
-                    </div>
-                    {exhibitors}
-                </>
-            );
+                        {exhibitors}
+                    </>
+                );
+            } else {
+                content = <>{exhibitors}</>;
+            }
         } else {
-            content = <>{exhibitors}</>;
+            content = (
+                <div className="booth__content -spec">
+                    {!!s.special.description}
+                    <div className="booth__desc" dangerouslySetInnerHTML={{ __html: s.special.description }} />
+                </div>
+            );
         }
-    } else {
-        content = (
-            <div className="booth__content -spec">
-                {!!s.special.description}
-                <div className="booth__desc" dangerouslySetInnerHTML={{ __html: s.special.description }} />
-            </div>
-        );
-    }
 
-    return (
-        <OverlayContent bar={bar} backMode="none" onClose={() => store.selectNone()}>
-            {content}
-        </OverlayContent>
-    );
-    // });
+        return (
+            <OverlayContent bar={bar} backMode="none" onClose={() => store.selectNone()}>
+                {content}
+            </OverlayContent>
+        );
+    });
 }
 
-export default () =>
-    useObserver(() => (!uiState.menu && uiState.details && uiState.details instanceof BoothBase ? <Booth /> : null));
+export default () => useObserver(() => (!uiState.menu && uiState.selectedBooth ? <Booth /> : null));
--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -6,20 +6,20 @@ import List from "./List";
 import "./Category.scss";
 
 function Category() {
-    // return useObserver(() => {
-    const bar = (
-        <div className="bar">
-            {uiState.selectedCategory.name}&nbsp;<span>({uiState.selectedCategory.exhibitors.length})</span>
-            <div className="note">Category</div>
-        </div>
-    );
+    return useObserver(() => {
+        const bar = (
+            <div className="bar">
+                {uiState.selectedCategory.name}&nbsp;<span>({uiState.selectedCategory.exhibitors.length})</span>
+                <div className="note">Category</div>
+            </div>
+        );
 
-    return (
-        <OverlayContent onClose={handleCloseBack} onBack={handleCloseBack} backMode="menu" bar={bar}>
-            <List />
-        </OverlayContent>
-    );
-    // });
+        return (
+            <OverlayContent onClose={handleCloseBack} onBack={handleCloseBack} backMode="menu" bar={bar}>
+                <List />
+            </OverlayContent>
+        );
+    });
 
     function handleCloseBack() {
         store.selectSearch();
