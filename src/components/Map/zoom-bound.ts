bc08afb7a8b9bac2419fa222cd48ea0205189f3f
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-10-30 23:19:54 +0300

    --wip-- [skip ci]

--- src/components/Map/zoom-bound.ts
+++ src/components/Map/zoom-bound.ts
@@ -2,6 +2,7 @@ import { Drawer } from "./drawing/Drawer1";
 import { ZoomTransform, zoomIdentity } from "d3-zoom";
 import { svgHeight, svgWidth } from "../../data/svg";
 import settings from "../../tools/settings";
+import { uiState } from "../../store";
 // import { MatrixReadonly } from "./drawing/Matrix";
 
 function zoomBound(drawer: Drawer, transform: ZoomTransform, forAutoMove: boolean) {
@@ -56,26 +57,36 @@ function zoomBoundBg(drawer: Drawer, transform: ZoomTransform, forAutoMove: bool
     const svgHeightUnscaled = svgHeight * scale;
     const svgWidthUnscaled = svgWidth * scale;
 
-    const vRect = drawer.getVisibleRect().scale(1 / drawer.pixelRatio);
+    let vRect1 = drawer.getVisibleRect().scale(1 / drawer.pixelRatio);
+    const vRect = uiState.screenSize;//.scale(1 / drawer.pixelRatio);
+    const cy = vRect.height / 2;
+    const cx = vRect.width / 2;
 
     const svgHeightScaled = svgHeightUnscaled * transform.k;
     const svgWidthScaled = svgWidthUnscaled * transform.k;
 
-    // calc center zoom tx/ty
-    const centerTy = -vRect.cy * (transform.k - 1);
-    const centerTx = -vRect.cx * (transform.k - 1);
-    // forAutoMove = true;
-    const extra = forAutoMove ? 0.1 : 0.5;
+    // const vRect = uiState.canvasVisibleRectPx.scale(1 / drawer.pixelRatio);
 
-    const allowShiftYBase = (svgHeightScaled - vRect.h) / 2;
+    //console.log("za", vRect1.h, vRect1.w, vRect.height, vRect.width, svgWidthUnscaled, svgHeightUnscaled, svgWidthScaled, svgHeightScaled);
+
+    // calc center zoom tx/ty
+    const centerTy = -cy * (transform.k - 1);
+    const centerTx = -cx * (transform.k - 1);
+    forAutoMove = true;
+
+    const extra = 0; //forAutoMove ? 0.1 : 0.5;
+
+    const allowShiftYBase = (svgHeightScaled - vRect.height) / 2;
     const allowShiftY = forAutoMove ? Math.max(allowShiftYBase, 0) : Math.abs(allowShiftYBase);
-    const maxDeltaY = allowShiftY + Math.min(vRect.h, svgHeightScaled) * extra;
+    const maxDeltaY = Math.max(allowShiftYBase, 0); //allowShiftY + Math.min(vRect.height, svgHeightScaled) * extra;
     const maxTy = centerTy + maxDeltaY;
     const minTy = centerTy - maxDeltaY;
 
-    const allowShiftXBase = (svgWidthScaled - vRect.w) / 2;
+    console.log("za", svgHeightScaled, vRect.height, allowShiftYBase, transform.y - centerTy, vRect.height / 2, svgHeightScaled / 2);
+
+    const allowShiftXBase = (svgWidthScaled - vRect.width) / 2;
     const allowShiftX = forAutoMove ? Math.max(allowShiftXBase, 0) : Math.abs(allowShiftXBase);
-    const maxDeltaX = allowShiftX + Math.min(vRect.w, svgWidthScaled) * extra;
+    const maxDeltaX = allowShiftX + Math.min(vRect.width, svgWidthScaled) * extra;
     const maxTx = centerTx + maxDeltaX;
     const minTx = centerTx - maxDeltaX;
 
