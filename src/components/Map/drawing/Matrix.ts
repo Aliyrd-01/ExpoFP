3276476053a9a5cfc988cb454413e986c392bae4
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-10-30 15:16:14 +0300

    --wip-- [skip ci]

--- src/components/Map/drawing/Matrix.ts
+++ src/components/Map/drawing/Matrix.ts
@@ -13,8 +13,8 @@ let svgCenterX = svgWidth / 2;
 let svgCenterY = svgHeight / 2;
 
 if (settings.EXPO === "eventtechlive2019" && svgWidth > 3000) {
-    const center = [3424, 2149];
-    const size = [845, 799];
+    const center = [3173, 1967];
+    const size = [1024, 873];
     svgCenterX = center[0];
     svgCenterY = center[1];
     svgVisibleHeight = size[0];
