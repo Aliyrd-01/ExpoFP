4afb99b0d49b98aeafa9016d132750a707a352a6
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-11-19 20:31:39 +0300

    --wip-- [skip ci]

---
+++ src/expofp.tsx
@@ -0,0 +1,10 @@
+import "array-flat-polyfill";
+
+// export ExpoFP as constructor - it will be declare as global var
+class ExpoFP {
+    a1() {
+        alert("a1");
+    }
+}
+
+alert(1);
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+  mode: isProd ? "production" : "development",
+  entry: {
+    index: "./src/expofp.tsx"
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "[name].js",
+    library: "ExpoFp"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: "style-loader",
+            // options: {
+            //   insert: function insertAtTop(element) {
+            //     // eslint-disable-next-line no-underscore-dangle
+            //     window._efpAddStyle = element;
+            //     // var parent = document.querySelector("head");
+            //     // 
+            //     // var lastInsertedElement =
+            //     //   window._lastElementInsertedByStyleLoader;
+
+            //     // if (!lastInsertedElement) {
+            //     //   parent.insertBefore(element, parent.firstChild);
+            //     // } else if (lastInsertedElement.nextSibling) {
+            //     //   parent.insertBefore(element, lastInsertedElement.nextSibling);
+            //     // } else {
+            //     //   parent.appendChild(element);
+            //     // }
+
+            //     // // eslint-disable-next-line no-underscore-dangle
+            //     // window._lastElementInsertedByStyleLoader = element;
+            //   }
+            // }
+          },
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ExpoFP",
+      template: "src/index.html",
+      inject: "head"
+    }),
+    new ForkTsCheckerWebpackPlugin({ eslint: true, async: false }),
+    new CleanWebpackPlugin()
+  ]
+};
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin({ extractComments: false })]
+  };
+} else {
+  // for more information, see https://webpack.js.org/configuration/dev-server
+  config.devServer = {
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    stats: "minimal",
+    overlay: true
+  };
+}
+
+module.exports = config;
