e0991929d258054a4460c36260b251eb868a54fa
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-11-22 08:55:25 +0300

    --wip-- [skip ci]

--- scripts/deploy-index.js
+++ scripts/deploy-index.js
@@ -9,7 +9,7 @@ var cloudfront = new AWS.CloudFront({ apiVersion: "2019-03-26", credentials });
 // https://purge.jsdelivr.net/npm/expofp2@1/dist/*
 const publicPath = "https://cdn.jsdelivr.net/npm/expofp@0.0.4/dist/";
 const expos = null; //["eventtechlive2020"];
-const except = ["demo", "jtrade19"];
+const except = ["demo", "jtrade19", "_template_for_new_event_"];
 
 console.log("Deploy to selected locations", publicPath, expos);
 
