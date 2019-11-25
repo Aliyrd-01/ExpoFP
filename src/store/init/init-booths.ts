68a542100f7051db383eb80bcadc6fea6968ab13
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-11-25 19:37:46 +0300

    --wip-- [skip ci]

--- src/store/init/init-booths.ts
+++ src/store/init/init-booths.ts
@@ -90,7 +90,7 @@ export default function initBooths(store: RootStore) {
         } else {
             boothSpec.color = el.getAttribute("data-color") || boothSpec.color;
         }
-        booth.description = booth.description || el.getAttribute("data-description"); // || '<b>Or do this</b>';
+        booth.description = booth.description;// || el.getAttribute("data-description"); // || '<b>Or do this</b>';
 
         const transform = rect.getAttribute("transform");
         if (transform) {
