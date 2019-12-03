853fce846eec727ea514e220c341911d9f8de42a
Evgeny Tyukin <evgeny.tukin@gmail.com>
2019-12-03 20:28:24 +0300

    --wip-- [skip ci]

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ const async = require("async");
 
 const stable = "0.1.29";
 const beta = "0.1.29";
-const alpha = "0.1.31"; //require("./package.json").version;
+const alpha = "0.1.32"; //require("./package.json").version;
 const minDaysUsed = 30;
 
 const betas = ["eventtechlive2020", "_template_for_new_event_", "eventscase", "expo"];
