57f7f53aa3773df4dad233b3bdaaf905d0213b8d
Evgeny Tukin <evgeny.tukin@9rays.net>
2019-12-04 16:31:47 +0300

    --wip--

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const AWS = require("aws-sdk");
 const async = require("async");
 
-const stable = "0.1.36";
-const beta = "0.1.36";
+const stable = "0.1.44";
+const beta = stable;//"0.1.44";
 const alpha = require("../package.json").version;
 const minDaysUsed = 30;
 
