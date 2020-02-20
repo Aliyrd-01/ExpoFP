3a9787dab7636b126f410bba6d99da36e558bb3b
Evgeny Tyukin <evgeny.tukin@gmail.com>
2020-02-20 10:00:48 +0300

    --wip-- [skip ci]

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ const alpha = require("../package.json").version;
 const minDaysUsed = 30;
 
 const betas = ["eventtechlive2020", "miblive2020", "_template_for_new_event_", "demo"];
-const alphas = ["thinksoft", "confex20"];
+const alphas = ["thinksoft", "confex20", "confex21", "sydneybuildexpo"];
 const force = [...alphas];
 
 const credentials = new AWS.SharedIniFileCredentials({ profile: "efp-deploy-fp" });
