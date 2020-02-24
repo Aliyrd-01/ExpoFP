1dc7a7268242c41890baa05bf6a5122a370a6d32
Evgeny Tyukin <evgeny.tukin@gmail.com>
2020-02-24 09:23:35 +0300

    -

--- scripts/create-package.js
+++ scripts/create-package.js
@@ -22,6 +22,8 @@ async function main() {
         let text = await res.text();
 
         text = text.replace(/https:\/\/cdn\.jsdelivr\.net\/npm\/expofp@([^/]+)\/dist\//g, "");
+        text = text.replace(/https:\/\/[^\.]+\.expofp\.com\/npm\/expofp@([^/]+)\/dist\//g, "");
+        text = text.replace(/crossorigin="anonymous"/g, "");
         text = text.replace(/https:\/\/[^\.]+\.expofp\.com\/data\//g, "data/");
         text = text.replace(/( class="expofp-floorplan")/g, '$1 data-data-url="data/"');
         fs.writeFileSync(__dirname + "/../dist/index.html", text);
