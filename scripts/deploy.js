04f98e8496a6ea05f2e8a703bf1e08c42b835f8a
Evgeny Tyukin <evgeny.tukin@gmail.com>
2020-02-25 11:18:11 +0000

    --wip-- [skip ci]

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 
 const stable = "2.0.1";
 // const updateToBeta = "0.4.3";
-const beta = "2.0.5";
+const beta = "2.0.16";
 const alpha = require("../package.json").version;
 const minDaysUsed = 30;
 
