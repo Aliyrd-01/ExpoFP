4535126f3b066a9142e787acccb77c38f1b1194b
Michael S <lymanlyman12@gmail.com>
2020-10-19 17:07:59 +0400

    [EFP-532] Use compression on preview

--- src/utils/preview-exhibitor.ts
+++ src/utils/preview-exhibitor.ts
@@ -1,6 +1,20 @@
+import lzUtils from "./lz-utils";
+
 const url = new URL(window.location.href);
-const c = url.searchParams.get("__data");
-const exhibitor: RawExhibitor = c ? JSON.parse(c).exhibitors[0] : null;
+
+var exhibitor: RawExhibitor = null;
+
+const data = url.searchParams.get("__data");
+if (data){
+    exhibitor = JSON.parse(data).exhibitors[0]
+}
+else{
+    const lzdata = url.searchParams.get("__lzdata");
+    if (lzdata){
+        const decompressedData = lzUtils.decompress(lzdata);
+        exhibitor = JSON.parse(decompressedData).exhibitors[0]; 
+    }
+}
 
 export default exhibitor;
 
