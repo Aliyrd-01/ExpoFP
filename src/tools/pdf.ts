8e539f238f3410bf5d1917772a5229090f4205fa
Michael S <lymanlyman12@gmail.com>
2020-10-29 14:05:36 +0400

    [EFP-699] Save PDF on IOS

--- src/tools/pdf.ts
+++ src/tools/pdf.ts
@@ -127,14 +127,40 @@ export async function generatePdf() {
     // });
 
     {
-        const supportsTouch = "ontouchstart" in window || navigator.msMaxTouchPoints;
+        const fileName = slugify(data.title, { remove: /[*+~.()'"!:@]/g, lower: true }) + ".pdf";
+        const supportsTouch = "ontouchstart" in window;
         if (supportsTouch) {
             const blob = doc.output("blob");
-            window.open(URL.createObjectURL(blob), "_blank");
+            saveToFile(blob, fileName);
         } else {
             // Not works on some mobile devices
-            doc.save(slugify(data.title, { remove: /[*+~.()'"!:@]/g, lower: true }) + ".pdf");
+            doc.save(fileName);
         }
+    }
+
+    function saveToFile(blob: Blob, fileName: string) {
+        // It is necessary to create a new blob object with mime-type explicitly set
+        // otherwise only Chrome works like it should
+        var newBlob = new Blob([blob], { type: "application/pdf" });
+
+        // IE doesn't allow using a blob object directly as link href
+        // instead it is necessary to use msSaveOrOpenBlob
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(newBlob);
+            return;
+        }
+
+        // For other browsers:
+        // Create a link pointing to the ObjectURL containing the blob.
+        const data = window.URL.createObjectURL(newBlob);
+        var link = document.createElement("a");
+        link.href = data;
+        link.download = fileName;
+        link.click();
+        setTimeout(function () {
+            // For Firefox it is necessary to delay revoking the ObjectURL
+            window.URL.revokeObjectURL(data);
+        }, 100);
     }
 
     function mmToPt(mm: number) {
