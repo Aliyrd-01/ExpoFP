e71daa39b939ca8295cbb85d3cc5304f535bd2ab
Aslan <aslan.kystaubayev@gmail.com>
2021-10-08 12:22:46 +0600

    [EFP-1334]: Add Reserve button term

--- src/components/Booth.tsx
+++ src/components/Booth.tsx
@@ -36,7 +36,7 @@ function Booth() {
             }
         },
         get reserveTitle() {
-            return t("Reserve");
+            return data.reserveButtonTerm || t("Reserve");
         },
         get descriptionCombined() {
             return this.booth.description || data.reserveInstructions || "";
