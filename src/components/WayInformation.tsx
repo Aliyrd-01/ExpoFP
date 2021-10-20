39268a09e6ad95b48a8df24224da31e7607e4b26
Evgeny Tyukin <evgeny.tukin@gmail.com>
2021-10-20 16:38:24 +0300

    Simplify WayInformation component

--- src/components/WayInformation.tsx
+++ src/components/WayInformation.tsx
@@ -6,26 +6,21 @@ export interface WayInformationItem {
     text: string;
 }
 export interface WayInformationProps {
-    items: Array<WayInformationItem>;
+    items: WayInformationItem[];
 }
 
-const WayInformation: React.FC<WayInformationProps> = ({ items = [] }) => {
-    return items.length ? (
-        <>
-            <div className="wayInformation">
-                {items.map((item, index) => {
-                    return (
-                        <div className="wayInformation__item" key={index}>
-                            <span>{item.title}</span>
-                            <strong>{item.text}</strong>
-                        </div>
-                    );
-                })}
-            </div>
-        </>
-    ) : (
-        <></>
-    );
-};
+const WayInformation: React.FC<WayInformationProps> = ({ items }) =>
+    items?.length ? (
+        <div className="wayInformation">
+            {items.map((item, index) => {
+                return (
+                    <div className="wayInformation__item" key={index}>
+                        <span>{item.title}</span>
+                        <strong>{item.text}</strong>
+                    </div>
+                );
+            })}
+        </div>
+    ) : null;
 
 export default WayInformation;
