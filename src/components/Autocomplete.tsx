d13aab2340b36838887c8018f4d731986887f6a9
Pavel Grishko <pavel@grishko.dev>
2021-10-26 00:51:05 +0300

    feat(Autocomplete component): add select by keys

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -19,15 +19,34 @@ const Autocomplete: React.FC<AutocompleteProps> = ({ placeholder, options }) => 
         if (showOptionsDropdown) setShowOptionsDropdown(false);
     });
 
-    const getActiveOptionIndex = (value) => {
+    const getActiveOptionIndexByValue = (value) => {
         return options.findIndex((option) => option === value);
     };
 
     const onClickOption = (event) => {
         setFilteredOptions([]);
         setInput(event.target.innerText);
-        setActiveOptionIndex(getActiveOptionIndex(event.target.innerText));
+        setActiveOptionIndex(getActiveOptionIndexByValue(event.target.innerText));
         setShowOptionsDropdown(false);
+    };
+
+    const onKeyDown = (event) => {
+        if (event.keyCode === 13) {
+            if (filteredOptions.length) {
+                const nextActiveIndex = getActiveOptionIndexByValue(filteredOptions[activeOptionIndex]);
+                setActiveOptionIndex(nextActiveIndex);
+                setInput(options[nextActiveIndex]);
+            } else {
+                setActiveOptionIndex(activeOptionIndex);
+                setInput(options[activeOptionIndex]);
+            }
+            setShowOptionsDropdown(false);
+        } else if (event.keyCode === 38) {
+            if (activeOptionIndex === 0) return;
+            setActiveOptionIndex(activeOptionIndex - 1);
+        } else if (event.keyCode === 40) {
+            activeOptionIndex !== null ? setActiveOptionIndex(activeOptionIndex + 1) : setActiveOptionIndex(0);
+        }
     };
 
     const onFocus = () => {
@@ -39,14 +58,15 @@ const Autocomplete: React.FC<AutocompleteProps> = ({ placeholder, options }) => 
         const result = options.filter((option) => option.toLowerCase().indexOf(userInput.toLowerCase()) > -1);
 
         setInput(event.target.value);
-        setFilteredOptions(result);
+        setFilteredOptions(result.length ? result : options);
         setActiveOptionIndex(null);
     };
 
-    const showOptions = (optionsList) => {
+    const showOptions = () => {
+        const allOptions = filteredOptions.length ? filteredOptions : options;
         return (
             <ul>
-                {optionsList.map((option, index) => {
+                {allOptions.map((option, index) => {
                     let activeClass;
                     if (index === activeOptionIndex) activeClass = "is-active";
                     return (
@@ -67,12 +87,11 @@ const Autocomplete: React.FC<AutocompleteProps> = ({ placeholder, options }) => 
                     type="text"
                     onChange={onChange}
                     onFocus={onFocus}
+                    onKeyDown={onKeyDown}
                     value={input}
                     placeholder={placeholder}
                 />
-                <div className="autocomplete__options">
-                    {filteredOptions.length ? showOptions(filteredOptions) : showOptions(options)}
-                </div>
+                <div className="autocomplete__options">{showOptions()}</div>
             </div>
         </>
     );
