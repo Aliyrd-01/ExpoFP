464a56d49986e65f8f109e47abc849dbff894706
Pavel Grishko <pavel@grishko.dev>
2021-11-12 14:59:00 +0200

    refactor(autocomplete): fix init value label in object mode

--- src/components/stories/Autocomplete.stories.tsx
+++ src/components/stories/Autocomplete.stories.tsx
@@ -8,7 +8,7 @@ export default {
 } as Meta;
 
 const Template: Story<AutocompleteProps> = (args) => {
-    const [value, setValue] = useState<string>(null);
+    const [value, setValue] = useState<string>("01.351");
     return (
         <>
             <Autocomplete {...args} onChange={setValue} value={value} />
@@ -22,7 +22,7 @@ const Template: Story<AutocompleteProps> = (args) => {
 export const arrayOfStrings = Template.bind({});
 arrayOfStrings.args = {
     placeholder: "Select Direction from",
-    options: ["one1", "one2", "one3", "two1", "two2", "three1", "three2", "four", "four2", "five", "five2", "six", "six2"],
+    options: ["one1", "one2", "01.351", "two1", "two2", "three1", "three2", "four", "four2", "five", "five2", "six", "six2"],
 };
 
 export const arrayOfObjects = Template.bind({});
@@ -36,6 +36,14 @@ arrayOfObjects.args = {
         {
             value: "1403",
             label: "EventMobi - 1403",
+        },
+        {
+            value: "01.351",
+            label: "Lumishore - 01.351",
+        },
+        {
+            value: "01.351",
+            label: "Navico - 01.152",
         },
     ],
 };
