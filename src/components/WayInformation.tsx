f3131f8f7ce305281640454afb2ce707a9968b7b
Pavel Grishko <pavel@grishko.dev>
2021-12-06 16:24:34 +0200

    feat(wayfinding): add accessible to wayfinding information

--- src/components/WayInformation.tsx
+++ src/components/WayInformation.tsx
@@ -1,22 +1,28 @@
 import React from "react";
+import classNames from "classnames";
 import store from "../store";
 import "./WayInformation.scss";
 
 export interface WayInformationItem {
     title: string;
     text: string;
-    accessible?: boolean;
 }
 export interface WayInformationProps {
     items: WayInformationItem[];
+    accessible?: boolean;
 }
 
-const WayInformation: React.FC<WayInformationProps> = ({ items }) =>
+const WayInformation: React.FC<WayInformationProps> = ({ items, accessible }) =>
     items?.length ? (
         <div className="wayInformation" onClick={() => store.showOverlay()}>
             {items.map((item, index) => {
                 return (
-                    <div className="wayInformation__item" key={index}>
+                    <div
+                        className={classNames("wayInformation__item", {
+                            "is-accessible": accessible && index === 0,
+                        })}
+                        key={index}
+                    >
                         <span>{item.title}</span>
                         <strong>{item.text}</strong>
                     </div>
