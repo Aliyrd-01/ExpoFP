778ae7261f9c3034f87287f307bba0e4466438a9
Rodion Nikolav <rodion@thinksoft.ru>
2021-12-22 08:21:17 +0300

    Fix label sizes

--- src/components/Map/drawing/config/config-booth-labels.ts
+++ src/components/Map/drawing/config/config-booth-labels.ts
@@ -91,11 +91,11 @@ class BoothLabelDrawer extends BoothDrawerBase<RectPainter> {
         let exh = (this.booth as RegularBooth).exhibitors.length > 0;
 
         this.addLabel(4 * context.pixelRatio, "PDF", pad / 8, true);
-        this.addLabel(exh ? 11 : 17 * context.pixelRatio, "S", pad / 8, true);
-        this.addLabel(exh ? 11 : 17 * context.pixelRatio, "M", pad / 8, false);
-        this.addLabel(exh ? 12 : 18 * context.pixelRatio, "L", pad, false);
-        this.addLabel(exh ? 13 : 19 * context.pixelRatio, "XL", pad, false);
-        
+        this.addLabel((exh ? 11 : 17) * context.pixelRatio, "S", pad / 8, true);
+        this.addLabel((exh ? 11 : 17) * context.pixelRatio, "M", pad / 8, false);
+        this.addLabel((exh ? 12 : 18) * context.pixelRatio, "L", pad, false);
+        this.addLabel((exh ? 13 : 19) * context.pixelRatio, "XL", pad, false);
+
         this.calcFactors();
         this.update();
 
