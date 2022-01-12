412ae8c47cf5128d0c31d9275e51d087c41901ae
Aslan <aslan.kystaubayev@gmail.com>
2022-01-12 18:39:45 +0600

    [EFP-1451]: Remove booths sort on FP

--- src/store/init/init-booths.ts
+++ src/store/init/init-booths.ts
@@ -168,12 +168,6 @@ export default function initBooths(store: RootStore) {
             boothStore.booths.push(b as Booth);
         }
     }
-    // sort booths by name
-    boothStore.booths.sort(function (a, b) {
-        var x = a.slug;
-        var y = b.slug;
-        return x < y ? -1 : x > y ? 1 : 0;
-    });
 
     // dispose
     delete data.booths;
