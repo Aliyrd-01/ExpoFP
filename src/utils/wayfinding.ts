a6c1d25f8b9b1007d554611fe26f2ed71ded7e3f
Rodion Nikolav <rodion@thinksoft.ru>
2022-01-21 15:19:51 +0300

    Fix route points simplification

--- src/utils/wayfinding.ts
+++ src/utils/wayfinding.ts
@@ -414,7 +414,7 @@ export const getGraphLines = (
         const prevAngle = prevLine ? lineAngle(prevLine.p0, prevLine.p1) : null;
         const angle = lineAngle(l.p0, l.p1);
 
-        if (!prevLine || prevLine.virtual !== l.virtual || Math.abs(Math.abs(angle) - Math.abs(prevAngle)) > 5) lines.push(l);
+        if (!prevLine || prevLine.virtual !== l.virtual || Math.abs(angle - prevAngle) > 5) lines.push(l);
         else prevLine.p1 = l.p1;
     }
 
