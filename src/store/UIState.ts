b02ebd5b1825aa72813c02847e859b58267ad9f0
Rodion Nikolav <rodion@thinksoft.ru>
2022-02-11 13:46:16 +0300

    disable dimming if no exhibitors.

--- src/store/UIState.ts
+++ src/store/UIState.ts
@@ -189,7 +189,7 @@ export default class UIState {
     // filtering
     @computed get dimmed() {
         return (
-            this.listItems.length &&
+            this.rootStore.exhibitorStore.exhibitors.length &&
             (this.listItems.length !== this.rootStore.exhibitorStore.exhibitors.length ||
                 this.listItems.find((x) => !(x instanceof Exhibitor)))
         );
