879cd23e89379a1b9e11cafff4a80f9031620b6b
Rodion Nikolav <rodion@thinksoft.ru>
2022-02-15 15:12:35 +0300

    Fix slug.ts function.

--- src/tools/slug.ts
+++ src/tools/slug.ts
@@ -16,7 +16,7 @@ export function generateUniqueSlug(text: string) {
     const slugBase = slugify(text, {remove: /[*+~.()'"!:@]/g, lower: true});
     let slug: string;
     do {
-        slug = slugBase + (!slugBase || append++ > 0 ? append : '');
+        slug = slugBase + ( append++ > 0 || !slugBase ? append : '');
     }
     while (generatedSlugs.has(slug))
 
