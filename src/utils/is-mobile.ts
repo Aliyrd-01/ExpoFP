454a886063212445e1315cbd436fbe0eb72798c0
SergLyss <serge@expofp.com>
2022-05-13 18:43:16 +0300

    added method for detect mobile device

---
+++ src/utils/is-mobile.ts
@@ -0,0 +1,9 @@
+function isMobile() {
+    let isMobile = false
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        isMobile = true
+    }
+    return isMobile
+}
+
+export default isMobile
