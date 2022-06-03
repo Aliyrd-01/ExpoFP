07ce21a882a68ca3247a0033670ba9a924609149
Rodion Nikolaev <rodion@screenshotmonitor.com>
2022-06-03 11:15:27 +0600

    Remove global variable

--- src/expofp.tsx
+++ src/expofp.tsx
@@ -10,7 +10,7 @@ window["__efpStyleElements"] = [];
 ready(() => {
     const floorplanDivs = document.querySelectorAll(".expofp-floorplan") as NodeListOf<HTMLDivElement>;
     for (const element of Array.from(floorplanDivs)) {
-        window["fp"] = new FloorPlanLoader({ element });
+        new FloorPlanLoader({ element });
     }
 });
 
