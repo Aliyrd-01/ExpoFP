7100690e1c69130f75568ccf632ffe2d7ba45e1f
EugAce <asdnvx14@yahoo.com>
2022-06-17 18:03:06 +0600

    Merge CC provided coordinates and New Pavel coordinates

--- src/components/Map/drawing/config/config-wf.ts
+++ src/components/Map/drawing/config/config-wf.ts
@@ -42,11 +42,11 @@ export function mapCurrentPosition(position: CurrentPosition): Point {
 
     if (settings.EXPO.indexOf("cannes") > -1) {
         mapping = {
-            "-1": { x: 10454, y: 12534 },
-            "0": { x: 10510, y: 9568 },
-            "1": { x: 10477, y: 7625 },
+            "-1": { x: 10460, y: 12534 },
+            "0": { x: 10511, y: 9568 },
+            "1": { x: 10480, y: 7625 },
             "3": { x: 10480, y: 5094 },
-            "4": { x: 10464, y: 3360 },
+            "4": { x: 10460, y: 3360 },
         };
 
         fpConfig = {
