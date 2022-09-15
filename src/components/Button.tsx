1f3ee0513b58e93f1f864ffde6c1fb4bc6465af6
Pavel Grishko <pavel@grishko.dev>
2022-09-15 15:56:39 +0300

    feat: change buttons inside sidebar, ui improvements, prefix for classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
-type variantTypes = "primary" | "secondary";
+type targets = "_self" | "_blank" | "_parent";
 
 export interface ButtonProps {
-    variant?: variantTypes;
-    text: string;
+    children?: ReactNode;
+    inline?: boolean;
+    text?: string;
+    link?: string;
+    target?: targets;
     disabled?: boolean;
-    onClick: () => void;
+    onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, disabled = false, onClick }) => {
-    return (
-        <button type="button" className="button-s" disabled={disabled} onClick={onClick}>
-            {text}
+const Button: React.FC<ButtonProps> = ({ children, inline = false, text, link, target = "_self", disabled = false, onClick }) => {
+    return link ? (
+        <a
+            href={link}
+            className={classNames("efp-button", { "efp-button--inline": inline })}
+            target={target}
+            rel="noopener noreferrer"
+            onClick={onClick}
+        >
+            {children ? children : text}
+        </a>
+    ) : (
+        <button
+            type="button"
+            className={classNames("efp-button", { "efp-button--inline": inline })}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            {children ? children : text}
         </button>
     );
 };
