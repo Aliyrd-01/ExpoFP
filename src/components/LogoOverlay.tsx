77665aaf51bc8788fa8c5cfa5aebc30061986f43
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2022-09-19 20:54:53 +0600

    Add warning icon

--- src/components/LogoOverlay.tsx
+++ src/components/LogoOverlay.tsx
@@ -4,6 +4,7 @@ import store, { uiState } from "../store";
 import settings from "../tools/settings";
 import { remsToPixels } from "../utils";
 import { t } from "../utils/i18n";
+import isFromDesigner from "../utils/is-from-designer";
 import "./LogoOverlay.scss";
 
 export default function LogoOverlay() {
@@ -20,16 +21,43 @@ export default function LogoOverlay() {
             style.opacity = uiState.wsStarted ? 1 : 0;
             return style;
         },
+
+        get warningStyle() {
+            const pad = uiState.overlayPosition === "left" ? remsToPixels(1) : remsToPixels(0.5);
+            let style: any;
+            if (uiState.overlayPosition === "left")
+                style = { bottom: uiState.mapVisibleBottom + 2 * pad + "px", right: pad + "px", width: "3rem" };
+            else {
+                style = { top: uiState.mapVisibleTop + 2 * pad + "px", right: pad + "px", width: "2rem" };
+                if (store.mapboxStore.mapBoxSelected) style.top = remsToPixels(0.5) + "px";
+            }
+            style.opacity = uiState.wsStarted ? 1 : 0;
+            return style;
+        },
     }));
 
     const bu = window["__efpBaseUrl"];
-    return useObserver(() =>
-        settings.EXPO === "jobbeursonline" ? ( // TODO: remove in future versions
-            <img className="logo-overlay" style={s.style} src={bu + "expofp-overlay.png"} alt={t("Made with ExpoFP")} />
-        ) : (
+
+    var dataSize = Math.round(window["__fpStat"]?.dataSize / 1024 / 1024 || 0);
+    var showWarning = isFromDesigner && dataSize > 9.5;
+
+    return useObserver(() => (
+        <div>
             <a href="https://expofp.com/" target="_blank" className="logo-overlay" style={s.style} rel="noopener noreferrer">
                 <img src={bu + "expofp-overlay.png"} alt={t("Made with ExpoFP")} />
             </a>
-        )
-    );
+            {showWarning && (
+                <a
+                    title={`File size ${dataSize}Mb. Maybe your plan is too slow.`}
+                    href="https://expofp.com/"
+                    target="_blank"
+                    className="logo-overlay"
+                    style={s.warningStyle}
+                    rel="noopener noreferrer"
+                >
+                    <img src={bu + "warning.png"} alt={t("Made with ExpoFP")} />
+                </a>
+            )}
+        </div>
+    ));
 }
