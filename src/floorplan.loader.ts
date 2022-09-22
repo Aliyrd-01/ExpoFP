6f90e34ea7c04602089cf5bae23d2db02fd96ae4
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2022-09-22 22:36:13 +0600

    Fix fpUrl logic

--- src/floorplan.loader.ts
+++ src/floorplan.loader.ts
@@ -60,7 +60,7 @@ export default class FloorPlanLoader implements FloorPlan {
         this.onBoothClick = options.onBoothClick;
         this.onDetails = options.onDetails;
         this.onFpConfigured = options.onFpConfigured;
-        this.onDirection = options.onDirection; 
+        this.onDirection = options.onDirection;
         this._ready = new Promise((resolve, reject) => {
             this.resolveReady = resolve;
         });
@@ -112,9 +112,7 @@ export default class FloorPlanLoader implements FloorPlan {
 
         const dataUrl = dataUrlBase + "data.js";
         const wfDataUrl = dataUrlBase + "wf.data.js";
-        const fpUrl = isFromDesigner
-            ? `https://efp-data.s3.amazonaws.com/expos/${eventId}/data/fp.svg.js`
-            : dataUrlBase + "fp.svg.js";
+        const fpUrl = dataUrlBase + "fp.svg.js";
 
         loadCss("vendor/fa/css/fontawesome-all.min.css", container);
         loadCss("vendor/sanitize-css/sanitize.css", container);
