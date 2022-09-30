fcce64048f119e09fc52706470ae942dd4bfe7a7
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2022-09-30 11:19:14 +0600

    Increment default maxZoomScale value

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -197,7 +197,7 @@ export default function Map() {
         </canvas>
     ));
 
-    function moveToRect(rect: Rect, maxZoomScale: number = 4, animate: boolean = true) {
+    function moveToRect(rect: Rect, maxZoomScale: number = 10, animate: boolean = true) {
         if (settings.EXPO === "springfair2022") maxZoomScale = 20;
         const zoomScale = zoomTransform(s.$canvas.node()).k; //m.getZoomTransform().k;
         const z = getTramsformToCenterSvgRect(rect, uiState.canvasVisibleRectPx, Math.max(zoomScale, maxZoomScale));
