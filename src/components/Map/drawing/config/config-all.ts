4d0cb2ff02b432538808b4a419edcbef44b18e52
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2022-10-24 12:32:06 +0600

    money2020usa fix. Do not show booths for hidden floors.

--- src/components/Map/drawing/config/config-all.ts
+++ src/components/Map/drawing/config/config-all.ts
@@ -41,6 +41,10 @@ export default function configAll(context: DrawerContext = _context): void {
         loadLayer(layer, layer.visible || layer === defaultLayer, context).then((configured) => {
             loaded++;
             if (counter === loaded) {
+
+                // TODO: money2020usa fix. Do no show booths for hidden floors.
+                if (store.layerStore.mode != LayersMode.Default)
+                    store.boothStore.booths = store.boothStore.booths.filter((b) => !!b.layer);
                 var l = [...uiState.selectedBooths][0]?.layer?.name || uiState.selectedRoute?.from?.layer.name;
                 if (l) store.layerStore.updateVisibility(l, true);
             }
