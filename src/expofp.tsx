cfa35169a1e15ce1bbcc571ad4d181d874290b7c
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2022-10-26 18:17:43 +0600

    Disable serviceworker regictration

--- src/expofp.tsx
+++ src/expofp.tsx
@@ -4,7 +4,7 @@ import FloorPlanLoader from "./floorplan.loader";
 import "./public-path.js";
 import reportError from "./tools/report-error";
 
-if ("serviceWorker" in navigator) navigator.serviceWorker.register("service-worker.js?v=4").then(() => {});
+//if ("serviceWorker" in navigator) navigator.serviceWorker.register("service-worker.js?v=4").then(() => {});
 
 window.addEventListener("error", reportError);
 window["__efpStyleElements"] = [];
