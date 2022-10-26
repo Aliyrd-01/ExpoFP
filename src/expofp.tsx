decb4807d8f6bb47f9cc938ef1ed853ccde2f658
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2022-10-26 14:48:06 +0600

    Update Service worker version

--- src/expofp.tsx
+++ src/expofp.tsx
@@ -8,7 +8,7 @@ window.addEventListener("error", reportError);
 window["__efpStyleElements"] = [];
 
 ready(async () => {
-    if ("serviceWorker" in navigator) await navigator.serviceWorker.register("service-worker.js");
+    if ("serviceWorker" in navigator) await navigator.serviceWorker.register("service-worker.js?v=3");
 
     const floorplanDivs = document.querySelectorAll(".expofp-floorplan") as NodeListOf<HTMLDivElement>;
     for (const element of Array.from(floorplanDivs)) {
