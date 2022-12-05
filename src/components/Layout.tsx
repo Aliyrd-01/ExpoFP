857f2d33b1311b9b646562b60580f987b355b360
Pavel Grishko <pavel@grishko.dev>
2022-12-05 16:00:57 +0200

    refactor(kiosk): fix WS, fix errors

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -40,7 +40,13 @@ export default observer(function Layout() {
     else if (data.expoFpAd) freeOrDemo = <Free />;
 
     return (
-        <div className={cn("layout", { "efp-kiosk": uiState.kiosk, "efp-layers-mode": store.layerStore.mode })}>
+        <div
+            className={cn("layout", {
+                "efp-kiosk": uiState.kiosk,
+                "efp-layers-mode": store.layerStore.mode,
+                "efp-ws-mode": uiState.wsShown,
+            })}
+        >
             <div className={`layout__fixed expo-${settings.EXPO} overlay-${store.uiState.overlayPosition}`}>
                 <Header />
                 <LogoOverlay />
