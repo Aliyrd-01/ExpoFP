9c0ef2be440548e5a95b40c38b89db8b167ef11e
SergLyss <serge@expofp.com>
2022-12-22 17:50:32 +0300

    location tracking after turning on geolocation in the device

--- src/floorplan.ready.tsx
+++ src/floorplan.ready.tsx
@@ -72,6 +72,7 @@ export default class FloorPlanReady extends FloorPlanLoader {
                     navigator.geolocation.clearWatch(watcher);
                     watcher = null;
                 }
+                this.trackGps();
             },
             {
                 maximumAge: 0,
