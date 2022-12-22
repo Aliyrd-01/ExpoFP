c813a60bc4908e6e62ce7716224f7b68e210bbc3
SergLyss <serge@expofp.com>
2022-12-22 18:56:02 +0300

    fix: changed the rotationAlignment option for the marker due to a bug with not displaying the marker on phones at an angle

--- src/components/Mapbox/utils/data.ts
+++ src/components/Mapbox/utils/data.ts
@@ -419,7 +419,8 @@ export function setMarker(type: "from" | "to" | "yah" | "cp", point: CurrentPosi
         var htmlElement = document.createElement("div");
         htmlElement.className = `marker ${type}`;
         marker = new mapboxgl.Marker(htmlElement, {
-            rotationAlignment: type === "cp" || type === "from" ? "map" : "auto",
+            rotationAlignment: "auto",
+            // rotationAlignment: type === "cp" || type === "from" ? "map" : "auto",
         }).setLngLat(lngLat);
 
         marker.addTo(map);
