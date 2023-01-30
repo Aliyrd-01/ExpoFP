d9fd5efff090222fc183f9459c1864784b8699e0
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2023-01-30 12:30:59 +0600

    Fix config

--- src/components/Map/drawing/config/config-wf.ts
+++ src/components/Map/drawing/config/config-wf.ts
@@ -80,14 +80,14 @@ export function mapCurrentPosition(position: CurrentPosition): Point {
     if (settings.EXPO === "demo") {
         fpConfig = {
             p0: { lat: 38.255223, lng: -85.756780, x: 3309, y: 2702},
-            p1: { lat: 38.253537, lng: -85.753878, x: 3799, y: 1725},
+            p2: { lat: 38.253537, lng: -85.753878, x: 3799, y: 1725},
         };
     }
         
     if (settings.EXPO === "bett2023") {
         fpConfig = {
             p0: { lat: 51.50924604464074, lng: 0.026175553161736653, x: 8629, y: 7416},
-            p1: { lat: 51.50715813053298, lng: 0.03452882241528408, x: 16272, y: 1033},
+            p2: { lat: 51.50715813053298, lng: 0.03452882241528408, x: 16272, y: 1033},
         };
     }
 
