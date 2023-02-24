56ac23df0f32593c308c91f474a451c33431bee7
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2023-02-24 13:47:22 +0600

    Add "View Map on Phone" text

--- src/components/LogoOverlay.tsx
+++ src/components/LogoOverlay.tsx
@@ -70,7 +70,11 @@ export default function LogoOverlay() {
                 </Alert>
             )}
             {uiState.kiosk && (
-                <div className="qr" style={{ bottom: remsToPixels(uiState.wsStarted ? 4.5 : 0.5), left: remsToPixels(0.5) }}>
+                <div
+                    className="qr"
+                    style={{ textAlign: "center", bottom: remsToPixels(uiState.wsStarted ? 4.5 : 0.5), left: remsToPixels(0.5) }}
+                >
+                    <div style={{ position: "relative", top: -5, fontSize: 12 }}>View Map on Phone</div>
                     <QRCode value={window.location.href} size={100} />
                 </div>
             )}
