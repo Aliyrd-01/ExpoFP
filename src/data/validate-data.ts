616ecd4d8700e64b01b2a5c27522443b74a46658
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2023-04-12 12:46:08 +0600

    Disable rebooking mode

--- src/data/validate-data.ts
+++ src/data/validate-data.ts
@@ -14,8 +14,6 @@ export default function validateData(data: Data) {
 
     if (data.hide3dMapDefault === undefined) data.hide3dMapDefault = false;
 
-    data.isRebooking = true;
-
     // temporary workaround for invalid data.js
     if (data.exhibitors.length > 0) {
         for (const booth of data.booths) {
