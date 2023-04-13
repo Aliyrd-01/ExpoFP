bc9bc0baa0d3ba840d737446f9e8ec3df8b06f5a
SergLyss <serge@expofp.com>
2023-04-13 15:32:06 +0300

    Add hideLogoOverlay param

--- src/data/Data.d.ts
+++ src/data/Data.d.ts
@@ -30,6 +30,7 @@ interface Data {
     hideRegisterToAttendLink: boolean;
     hideDownloadPdfLink: boolean;
     hideBookmarksLink: boolean;
+    hideLogoOverlay: boolean;
     autoTrackingGps: boolean;
     allow3dView: boolean;
     hide3dMapDefault: boolean;
