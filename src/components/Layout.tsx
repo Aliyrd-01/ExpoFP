238a26ea9cebe3504c3cfee51ca11135ac95d9dd
SergLyss <serge@expofp.com>
2023-04-18 18:22:13 +0300

    Revert: add hideLogoOverlay param

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -50,7 +50,8 @@ export default observer(function Layout() {
         >
             <div className={`layout__fixed expo-${settings.EXPO} overlay-${store.uiState.overlayPosition}`}>
                 <Header />
-                {!data.hideLogoOverlay && <LogoOverlay />}
+                {/*{!data.hideLogoOverlay && <LogoOverlay />}*/}
+                <LogoOverlay />
                 <Ws />
                 <Controls />
                 {/* <Layers /> */}
