0976bc473335355443a0380381a9f2f298cbd3bb
Pavel Grishko <pavel@grishko.dev>
2023-06-30 19:49:23 +0300

    refactor: change layers loading animation, improve floors switcher

--- src/components/LayersLoading.tsx
+++ src/components/LayersLoading.tsx
@@ -9,14 +9,9 @@ export interface LayersLoadingProps {
 const LayersLoading: React.FC<LayersLoadingProps> = ({ active }) => {
     return (
         <div className={cn("layers-loading", { "is-hidden": !active })}>
-            <div className="layers-loading-spinner">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+            <svg viewBox="25 25 50 50">
+                <circle r="20" cy="50" cx="50"></circle>
+            </svg>
         </div>
     );
 };
