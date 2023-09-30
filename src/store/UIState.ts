2d01e9a573ca7c2844e1f96c9c5e1bdf0aad98a5
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2023-09-30 16:57:48 +0600

    EFP-2870 Показывать сначала категории, потом список экзибиторв в FP

--- src/store/UIState.ts
+++ src/store/UIState.ts
@@ -256,24 +256,31 @@ export default class UIState {
         const boothsArray = boothStore.booths;
 
         if (!text) {
+            let combinedArray = [];
+            const cats = data.showCategories ? categoriesArray : [];
+
             const otherSpacesArray = boothsArray.filter((b) => b instanceof SpecialBooth);
-            const combinedArray = [...exhibitorsArray, ...otherSpacesArray];
+
+            if (data.showCompaniesAndBooths) combinedArray = combinedArray.concat(exhibitorsArray);
+            if (data.showOtherSpaces) combinedArray = combinedArray.concat(otherSpacesArray);
 
             return exhibitorsArray.length === 0
                 ? boothsArray
-                : combinedArray.sort((a, b) => {
-                      const aFeatured = a instanceof Exhibitor && a.featured !== undefined;
-                      const bFeatured = b instanceof Exhibitor && b.featured !== undefined;
+                : cats.concat(
+                      combinedArray.sort((a, b) => {
+                          const aFeatured = a instanceof Exhibitor && a.featured !== undefined;
+                          const bFeatured = b instanceof Exhibitor && b.featured !== undefined;
 
-                      if (aFeatured !== bFeatured) {
-                          return aFeatured ? -1 : 1;
-                      }
+                          if (aFeatured !== bFeatured) {
+                              return aFeatured ? -1 : 1;
+                          }
 
-                      const aDisplayName = a instanceof SpecialBooth && a.title ? a.title : a.name;
-                      const bDisplayName = b instanceof SpecialBooth && b.title ? b.title : b.name;
+                          const aDisplayName = a instanceof SpecialBooth && a.title ? a.title : a.name;
+                          const bDisplayName = b instanceof SpecialBooth && b.title ? b.title : b.name;
 
-                      return aDisplayName.localeCompare(bDisplayName, undefined, { sensitivity: "base" });
-                  });
+                          return aDisplayName.localeCompare(bDisplayName, undefined, { sensitivity: "base" });
+                      })
+                  );
         }
         if (text === "testerror") throw new Error("Test error");
         if (text === "2testerror") {
