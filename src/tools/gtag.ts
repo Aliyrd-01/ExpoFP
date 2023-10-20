1a32bff31e1a9bbc38f2791a58bd36d76234c5fc
Eugene A <asdnvx14@yahoo.com>
2023-10-20 14:13:12 +0200

    Fix set consent from localhstorage

--- src/tools/gtag.ts
+++ src/tools/gtag.ts
@@ -39,7 +39,7 @@ function hasUserConsent(allowConsent?: boolean): "granted" | "denied" | undefine
     if (allowConsent === false || allowConsent === true) return allowConsent ? "granted" : "denied";
 
     // if allowConsent === undefined
-    if (isLocalStorageAvailable ?? allowConsent !== undefined)
+    if (isLocalStorageAvailable && allowConsent !== undefined)
         return localStorage.getItem("userCookieChoice") === "true" ? "granted" : "denied";
 
     return undefined;
