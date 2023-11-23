a730d342f7565528ab339e5d12c403ce56c34fd2
SergLyss <serge@expofp.com>
2023-11-23 17:23:00 +0300

    Remove passing ip to check gdpr

--- src/tools/gtag.ts
+++ src/tools/gtag.ts
@@ -34,38 +34,21 @@ export enum GaEventActions {
     ClickDirections = "Click Directions",
 }
 
-async function getIp(): Promise<string | null> {
-    const response = await fetch('https://api.ipify.org?format=json');
-    if (response.ok) {
-        const data = await response.json();
-        return data.ip;
-    }
-
-    return null;
-}
-
 export async function checkUserIsGDPR(): Promise<boolean | null> {
-    try {
-        const ip = await getIp();
-        if (ip) {
-            const fetchPromise = fetch(`https://expofp-verify-ip-65e97c18c089.herokuapp.com/api/verify-ip/is-in-gdpr?ip=${ip}`, {
-                method: 'GET',
-            });
+    const fetchPromise = fetch(`https://consent.expofp.com/api/verify-ip/is-in-gdpr`);
 
-            const timeoutPromise = new Promise<boolean | null>((resolve) => {
-                setTimeout(() => {
-                    resolve(null);
-                }, 5000);
-            });
+    const timeoutPromise = new Promise<boolean | null>((resolve) => {
+        setTimeout(() => {
+            resolve(null);
+        }, 5000);
+    });
 
-            const response = await Promise.race([fetchPromise, timeoutPromise]);
+    const response = await Promise.race([fetchPromise, timeoutPromise]);
 
-            if (response instanceof Response && response.ok) {
-                const data: { result: boolean } = await response.json();
-                return data.result;
-            }
-        }
-    } catch (err) {}
+    if (response instanceof Response && response.ok) {
+        const data: { result: boolean } = await response.json();
+        return data.result;
+    }
 
     return null;
 }
