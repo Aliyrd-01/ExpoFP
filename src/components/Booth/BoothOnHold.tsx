236c093c9448496a87bb8447481c10199ff07b81
PlatonZvonkov <222reviews@gmail.com>
2023-12-12 00:01:07 +0300

    EFP-3140 modified BoothOnHold

--- src/components/Booth/BoothOnHold.tsx
+++ src/components/Booth/BoothOnHold.tsx
@@ -1,10 +1,43 @@
 import { t } from "../../utils/i18n";
-import React from "react";
+import React, { FC } from "react";
+import { RegularBooth } from "../../store/BoothStore";
+import data from "../../data";
 
-export const BoothOnHold = () => {
+interface BoothWithoutExhibitorProps {
+    booth: RegularBooth;
+    description: string;
+    showBuy: boolean;
+    showReserve: boolean;
+    isRebooking: boolean;
+}
+
+export const BoothOnHold: FC<BoothWithoutExhibitorProps> = ({ booth }) => {
     return (
         <div className="booth__content -reg">
-            <div>{t("On Hold")}</div>
+            <div className="booth__infos">
+                {booth.type && (
+                    <div className="booth__info">
+                        <i className="fas fa-cube" />
+                        <div className="booth__info-title">{t("{{boothTerm}} Type", { boothTerm: data.boothTerm })}</div>
+                        <div className="booth__info-val">{booth.type}</div>
+                    </div>
+                )}
+                {booth.size && (
+                    <div className="booth__info">
+                        <i className="fas fa-expand-alt" />
+                        <div className="booth__info-title">{t("Size")}</div>
+                        <div className="booth__info-val">{booth.size}</div>
+                    </div>
+                )}
+                {booth.price && booth.price !== "0" && (
+                    <div className="booth__info">
+                        <i className="fas fa-tag" />
+                        <div className="booth__info-title">{t("Price")}</div>
+                        <div className="booth__info-val">{booth.price}</div>
+                    </div>
+                )}
+            </div>
+            <div className="booth__content -spec">{t("On Hold")}</div>
         </div>
     );
 };
