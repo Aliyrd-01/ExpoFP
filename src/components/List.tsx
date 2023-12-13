5e192e51485de8fb4c445874572a3284b4ec68d0
SergLyss <serge@expofp.com>
2023-12-13 16:56:56 +0300

    wip

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -47,6 +47,7 @@ export default function List({ updatedScrollableRef, updateScroll }: ListProps) 
                     className="list-virtual"
                     style={{ minHeight: uiState.listItems.length ? "1px" : 0 }}
                     ref={listRef}
+                    defaultItemHeight={0}
                     itemContent={(index) => mapItem({ index })}
                     totalListHeightChanged={() => updateScroll && updateScroll()}
                     customScrollParent={scrollableRef.current}
