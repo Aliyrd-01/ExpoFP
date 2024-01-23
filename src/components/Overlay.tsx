35f2172409f6af14312d20120467a718353e8c38
SergLyss <serge@expofp.com>
2024-01-23 16:37:52 +0300

    Fix overlay bug

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -99,8 +99,8 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
             if (!rt) return;
             let diff = s.startedTouch.clientY - rt.clientY;
             // if (this.negateMove) diff = -diff;
-            const current = getTopForBottomPosition(uiState.overlaySize);
-            const medium = getTopForBottomPosition("medium");
+            const current = getTopForBottomPosition(uiState.overlaySize, el.current);
+            const medium = getTopForBottomPosition("medium", el.current);
             let newSize = uiState.overlaySize;
             if (diff < 0) {
                 if (uiState.overlaySize === "medium" || current + diff > medium) newSize = "small";
@@ -152,7 +152,7 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
         }
 
         function setShowAll() {
-            const all = uiState.overlayPosition === "left" || getTopForBottomPosition("full") + "px" === el.current.style.top;
+            const all = uiState.overlayPosition === "left" || getTopForBottomPosition("full", el.current) + "px" === el.current.style.top;
             uiState.overlayShowsAll = all;
         }
 
@@ -161,13 +161,13 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
             // let's animate when no touch in progress
             if (uiState.overlayPosition === "left") return;
 
-            let newTop = getTopForBottomPosition(uiState.overlaySize);
+            let newTop = getTopForBottomPosition(uiState.overlaySize, el.current);
 
             let transition = true;
             if (s.touchDiff !== undefined) {
                 newTop -= s.touchDiff;
-                const maxTop = getTopForBottomPosition("small");
-                const minTop = getTopForBottomPosition("full");
+                const maxTop = getTopForBottomPosition("small", el.current);
+                const minTop = getTopForBottomPosition("full", el.current);
                 newTop = Math.min(Math.max(newTop, minTop), maxTop);
                 transition = false;
             } else if (s.currentTop === undefined) {
@@ -221,15 +221,15 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
 
 const miniSizeRems = 3.5;
 const paddingRems = 2;
-function getTopForBottomPosition(size: OverlaySize): number {
-    // const containerHeight = el.parentElement.getBoundingClientRect().height;
+function getTopForBottomPosition(size: OverlaySize, el: HTMLDivElement): number {
+    const containerHeight = el.parentElement.getBoundingClientRect().height;
     switch (size) {
         case "full":
             return remsToPixels(paddingRems);
         case "medium":
-            return uiState.rootElement.clientHeight - remsToPixels(uiState.overlayMediumHeightRems);
+            return containerHeight - remsToPixels(uiState.overlayMediumHeightRems);
         case "small":
-            return uiState.rootElement.clientHeight - remsToPixels(miniSizeRems);
+            return containerHeight - remsToPixels(miniSizeRems);
     }
 
     return null;
