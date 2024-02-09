38fc07bd80b0028d32ab070cf101a6e4475e4e9c
SergLyss <serge@expofp.com>
2024-02-09 21:29:52 +0300

    Selecting a booth/exhibitor by its slug/externalId is now case insensitive by url

--- src/services/routing.ts
+++ src/services/routing.ts
@@ -52,7 +52,9 @@ export function initRouting(offHistory = false) {
         const slug = history.location.search.length > 1 ? decodeURIComponent(history.location.search.substring(1)) : "";
         disableStateToUrl = true;
 
-        const booth = store.boothStore.booths.find((x: Booth) => x.slug === slug || x.externalId === slug);
+        const booth = store.boothStore.booths.find(
+            (x: Booth) => x.slug.toLowerCase() === slug.toLowerCase() || x.externalId.toLowerCase() === slug.toLowerCase()
+        );
 
         if (hanleCustomCommand(slug, false)) {
         } else if (slug.startsWith("route")) {
@@ -75,7 +77,7 @@ export function initRouting(offHistory = false) {
                 () => store.layerStore.layersLoaded,
                 () => {
                     const exhibitor = store.exhibitorStore.exhibitors.find(
-                        (x: Exhibitor) => x.slug === slug || x.externalId === slug
+                        (x: Exhibitor) => x.slug.toLowerCase() === slug.toLowerCase() || x.externalId.toLowerCase() === slug.toLowerCase()
                     );
                     if (exhibitor) setTimeout(() => store.clickExhibitor(exhibitor), 250);
                     else {
