7972295547af87a8e34eec01a504b98237ca834c
SergLyss <serge@expofp.com>
2024-02-27 19:13:13 +0300

    The rules apply to all devices

--- src/components/Map/drawing/config/canvases.ts
+++ src/components/Map/drawing/config/canvases.ts
@@ -162,31 +162,21 @@ export function createExhibitorsDetailsCanvas(
 
     const exhibitorsWithOrder = b.exhibitors.filter((e) => e.order);
 
-    if (!isMobile) {
-        if (primaryExhibitors.length > 0) {
-            mainLines.push(...primaryExhibitors.map((e) => e.name));
-        } else if (onlyFeaturedExhibitors) {
-            mainLines.push(...b.exhibitors.filter((e) => e.featured).map((e) => e.name));
-        } else {
-            mainLines.push(...b.exhibitors.map((e) => e.name));
+    if (primaryExhibitors.length > 0) {
+        mainLines.push(...primaryExhibitors.map((e) => e.name));
+    } else if (onlyFeaturedExhibitors) {
+        mainLines.push(...b.exhibitors.filter((e) => e.featured).map((e) => e.name));
+    } else if (exhibitorsWithOrder.length > 0) {
+        mainLines.push(...exhibitorsWithOrder.map((e) => e.name));
+        const exhibitorsWithoutOrder = b.exhibitors.filter((e) => e.order === undefined);
+        if (exhibitorsWithoutOrder.length > 0) {
+            mainLines.push(`and ${exhibitorsWithoutOrder.length} more`);
         }
     } else {
-        if (primaryExhibitors.length > 0) {
-            mainLines.push(...primaryExhibitors.map((e) => e.name));
-        } else if (onlyFeaturedExhibitors) {
-            mainLines.push(...b.exhibitors.filter((e) => e.featured).map((e) => e.name));
-        } else if (exhibitorsWithOrder.length > 0) {
-            mainLines.push(...exhibitorsWithOrder.map((e) => e.name));
-            const exhibitorsWithoutOrder = b.exhibitors.filter((e) => e.order === undefined);
-            if (exhibitorsWithoutOrder.length > 0) {
-                mainLines.push(`and ${exhibitorsWithoutOrder.length} more`);
-            }
+        if (b.exhibitors.length > 5) {
+            mainLines.push(`${b.exhibitors.length} exhibitors`);
         } else {
-            if (b.exhibitors.length > 5) {
-                mainLines.push(`${b.exhibitors.length} exhibitors`);
-            } else {
-                mainLines.push(...b.exhibitors.map((e) => e.name));
-            }
+            mainLines.push(...b.exhibitors.map((e) => e.name));
         }
     }
 
