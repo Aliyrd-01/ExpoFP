f1661f95718dc44846e727d6200b183ecf75df25
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2024-05-21 17:13:43 +0500

    Fix Mapbox style URL in init-mapbox.ts

--- src/components/Threejs/mapbox/init-mapbox.ts
+++ src/components/Threejs/mapbox/init-mapbox.ts
@@ -10,8 +10,8 @@ export default function initMapbox(container: HTMLElement, style: string): Promi
 
         const map = new mapboxgl.Map({
             container: container,
-            style: `mapbox://styles/mapbox/${"light-v11"}`,
-            //style: "mapbox://styles/mapbox/" + style,
+            //style: `mapbox://styles/mapbox/${"light-v11"}`,
+            style: "mapbox://styles/mapbox/" + style,
             antialias: true,
             pitch: 30,
             maxPitch: 70,
