1775bd2c1ae6c6fe6761adcc670dd8d2fc9bb808
SergLyss <serge@expofp.com>
2024-05-24 16:41:17 +0300

    Add observable flags in UiState

--- src/store/UIState.ts
+++ src/store/UIState.ts
@@ -61,6 +61,11 @@ export default class UIState {
     @observable galleryActive = false;
     @observable hideOverlay = false;
     @observable hideCookieConsent = Boolean(hasUserConsent());
+    @observable hideHeaderLogo = false;
+    @observable hideLogoInBooth = false;
+    @observable disableFeatured = false;
+    @observable disableBookmarked = false;
+    @observable monochrome = false;
     @observable heatmap = false;
     rtl = getLanguage() === "ar" || getLanguage() === "he";
     rootElement: HTMLDivElement;
