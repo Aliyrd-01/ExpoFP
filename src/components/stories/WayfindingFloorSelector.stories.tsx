b1a455935da3ad017dc97a06babefeda8fc38b61
SergLyss <serge@expofp.com>
2024-06-24 14:42:24 +0300

    Fix stories

---
+++ src/components/stories/WayfindingFloorSelector.stories.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Meta, Story } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import WayfindingFloorSelector, { WayfindingFloorSelectorProps } from "../WayfindingFloorSelector";
+
+export default {
+    title: "Components/WayfindingFloorSelector",
+    component: WayfindingFloorSelector,
+} as Meta;
+
+const Template: Story<WayfindingFloorSelectorProps> = (args) => {
+    const [currentFloor, setCurrentFloor] = useState<{id: number, name: string}>(args.current);
+    const changeFloor = (val: {id: number, name: string}) => {
+        setCurrentFloor(val);
+        action("onClickFloor")(val);
+    };
+
+    return (
+        <div className="map layout">
+            <aside className="sidebar">
+                <WayfindingFloorSelector {...args} current={currentFloor} onClickFloor={changeFloor} />
+            </aside>
+        </div>
+    );
+};
+
+export const Base = Template.bind({});
+Base.args = {
+    floors: [
+        {
+            id: 1,
+            name: "1"
+        },
+        {
+            id: 2,
+            name: "2"
+        },
+        {
+            id: 3,
+            name: "3"
+        },
+        {
+            id: 4,
+            name: "4"
+        },
+        {
+            id: 5,
+            name: "5"
+        }
+    ],
+    current: {
+        id: 2,
+        name: "2"
+    },
+};
