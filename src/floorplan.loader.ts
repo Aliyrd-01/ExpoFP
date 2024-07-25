c0a0e47e80072e0696d87894b5065b0634b952ca
kda <mail@kda.name>
2024-07-25 14:56:14 -0600

    EFP-3978: add selectCategory

--- src/floorplan.loader.ts
+++ src/floorplan.loader.ts
@@ -98,6 +98,10 @@ export default class FloorPlanLoader implements FloorPlan {
         nr();
     }
 
+    selectCategory(slug: string): void {
+        nr();
+    }
+
     unstable_destroy(): void {
         nr();
     }
