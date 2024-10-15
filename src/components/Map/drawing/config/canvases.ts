7c3ec75bfb12c9e3206d6b6d280e5b9291d70cfc
kda <mail@kda.name>
2024-10-15 10:49:40 -0600

    fix: booths text

--- src/components/Map/drawing/config/canvases.ts
+++ src/components/Map/drawing/config/canvases.ts
@@ -186,10 +186,15 @@ export function createExhibitorsDetailsCanvas(
             mainLines.push(`and ${exhibitorsWithoutOrder.length} more`);
         }
     } else {
-        if (b.exhibitors.length > 5) {
+        const limit = data.viewOptimizationLevel ? 1 : 3;
+        const truncateText = (text) => text?.length > 10 ? `${text.slice(0, 10)}...` : text;
+
+        if (b.exhibitors.length > limit) {
             mainLines.push(`${b.exhibitors.length} ${data.exhibitorTermPlural}`);
         } else {
-            mainLines.push(...b.exhibitors.map((e) => e.name));
+            mainLines.push(...b.exhibitors.map(
+                (e) => data.viewOptimizationLevel ? truncateText(e.name) : e.name
+            ));
         }
     }
 
