c382d18f868409f6170f6aaae6df9322e933fb83
Evgeny <evgeny.tukin@gmail.com>
2024-10-20 15:31:15 +0200

    --wip-- [skip ci]

--- src/tools/webgl-logger.ts
+++ src/tools/webgl-logger.ts
@@ -2,14 +2,26 @@ import logger from "./logger";
 
 let loadedBytes = 0;
 let loggedMb = 0;
+const loggedByCategory = new Map<string, number>();
 
 export function logBuffer(bytes: number, category: string) {
     loadedBytes += bytes;
+    let categoryBytes = loggedByCategory.get(category) || 0;
+    loggedByCategory.set(category, categoryBytes + bytes);
+
     const loadedMbRounded = Math.floor(loadedBytes / 1024 / 1024);
     // log every 10MB
     if (loadedMbRounded > loggedMb + 10) {
         loggedMb = loadedMbRounded;
-        logger.log(`Buffers: ${loadedMbRounded} MB`);
+
+        const catLogMessage = Array.from(
+            loggedByCategory.entries().map(([key, value]) => {
+                const roundedMb = Math.floor(value / 1024 / 1024);
+                return `${key}: ${roundedMb}MB`;
+            })
+        ).join(", ");
+
+        logger.log(`GPU Allocated: ${loadedMbRounded}MB, ${catLogMessage}`);
     }
 }
 
