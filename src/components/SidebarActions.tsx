d355ede24cdb6c26368b61bece74816a47fa4c5c
Pashkella <hryshkodev@gmail.com>
2025-01-13 15:53:04 +0200

    feat: add ToggleButton (visited button)

--- src/components/SidebarActions.tsx
+++ src/components/SidebarActions.tsx
@@ -3,23 +3,30 @@ import classNames from "classnames";
 import "./SidebarActions.scss";
 import { t } from "../utils/i18n";
 import i18next from "i18next";
+import ToggleButton from "./ToggleButton";
 
 export interface SidebarActionsProps {
     inBookmark?: boolean;
+    visited?: boolean;
+    showVisited?: boolean;
     showBookmark?: boolean;
     showDirections?: boolean;
     showShare?: boolean;
     onClickBookmark?: () => void;
+    onClickVisited?: () => void;
     onClickDirections?: () => void;
     onClickShare?: () => void;
 }
 
 const SidebarActions: React.FC<SidebarActionsProps> = ({
     inBookmark = false,
+    visited = false,
+    showVisited = false,
     showBookmark = true,
     showDirections = true,
     showShare = true,
     onClickBookmark,
+    onClickVisited,
     onClickDirections,
     onClickShare,
 }) => {
@@ -31,6 +38,14 @@ const SidebarActions: React.FC<SidebarActionsProps> = ({
                     {t("Directions")}
                 </button>
             )}
+            {showVisited && (
+                <ToggleButton
+                    className="efp-visited-btn"
+                    toggled={visited}
+                    label={i18next.t("Visited")}
+                    onClick={onClickVisited ?? (() => {})}
+                />
+            )}
             {showBookmark && (
                 <button
                     type="button"
@@ -38,13 +53,11 @@ const SidebarActions: React.FC<SidebarActionsProps> = ({
                     onClick={onClickBookmark}
                 >
                     <i className={inBookmark ? "icon-bookmark-solid" : "icon-bookmark"}></i>
-                    {t("Bookmark")}
                 </button>
             )}
             {showShare && (
                 <button type="button" className="efp-actionButton efp-actionButton--share" onClick={onClickShare}>
                     <i className="icon-share"></i>
-                    {i18next.language === "en" && "Share"}
                 </button>
             )}
         </div>
