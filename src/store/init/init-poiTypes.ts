95017ac278318dfe88ccc9d18860705be8b7a3a8
Rodion Nikolaev <rodion@screenshotmonirtor.com>
2025-01-23 09:35:05 +0500

    Fix undefined poiTypes

--- src/store/init/init-poiTypes.ts
+++ src/store/init/init-poiTypes.ts
@@ -2,7 +2,7 @@ import data from "../../data";
 import RootStore from "../RootStore";
 
 export function initPoiTypes(store: RootStore) {
-    data.poiTypes.forEach((poiType) => {
+    (data.poiTypes ?? []).forEach((poiType) => {
         store.poiTypeStore.poiTypes.push(poiType);
     });
 }
