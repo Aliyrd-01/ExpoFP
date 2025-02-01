f8374a65d94ea216e5d4efb57aced814ff84c6b7
kda <mail@kda.name>
2025-01-31 19:25:26 -0600

    chore (EFP-4403): refactor

--- src/components/Map/drawing/config/config-all.ts
+++ src/components/Map/drawing/config/config-all.ts
@@ -52,7 +52,7 @@ export default function configAll(context: DrawerContext = _context): void {
             const key = "expofp_cache_refresh_ready";
 
             if (localStorage.getItem(key) === "1") {
-                store.fp.offlineManager.refreshCache()
+                store.fp.offlineManager.refreshCache();
             } else {
                 localStorage.setItem(key, "1");
             }
