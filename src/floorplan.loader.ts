147febeac599eb0c8cbeeba63d58a33ed291705f
kda <mail@kda.name>
2025-02-05 12:57:24 -0600

    chore: EFP-3874

--- src/floorplan.loader.ts
+++ src/floorplan.loader.ts
@@ -352,7 +352,6 @@ export default class FloorPlanLoader implements FloorPlan {
                         "transition": "icons/transition.svg",
                         "transition_up": "icons/transition_up.svg",
                         "transition_down": "icons/transition_down.svg",
-                        // "waypoint": "icons/waypoint.svg",
                     }).map(([key, path]) =>
                         loadImage(baseUrl ? new URL(path, baseUrl).href : path).then(image => [key, image] as [string, HTMLImageElement])
                     )
