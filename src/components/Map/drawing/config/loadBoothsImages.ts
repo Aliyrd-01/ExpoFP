3a6dff9df12b996d5e8c27e96011f462becd8c85
kda <mail@kda.name>
2025-02-12 12:10:39 -0600

    fix(EFP-4536): fix logo priority

--- src/components/Map/drawing/config/loadBoothsImages.ts
+++ src/components/Map/drawing/config/loadBoothsImages.ts
@@ -10,7 +10,7 @@ import { select } from "d3";
 import type { Layer } from "../../../../store/LayerStore";
 import isWebview from "../../../../utils/is-webview";
 import { getLogoUrl } from "../../../../utils/getLogoUrl";
-import { BOOTHS_PAINTER_MARKER, LAYER_ICONS_MARKER, LAYER_LOGOS_MARKER, SEPARATOR } from "../../../../constants";
+import { BOOKMARK_PAINTER_MARKER, BOOTHS_PAINTER_MARKER, LAYER_ICONS_MARKER, LAYER_LOGOS_MARKER, SEPARATOR } from "../../../../constants";
 import { areLayersEnabled } from "../../../../utils/areLayersEnabled";
 
 const CHUNK_SIZE = isMobile || isWebview ? 8 : 512;
@@ -37,7 +37,7 @@ export async function loadBoothsImages(context: DrawerContext, chunkSize = CHUNK
 
     const boothsPaintersById = new Map(
         context.allPainters
-            .filter(p => p.id.includes(BOOTHS_PAINTER_MARKER))
+            .filter(p => p.id.includes(BOOTHS_PAINTER_MARKER) && !p.id.includes(BOOKMARK_PAINTER_MARKER))
             .map(p => [
                 p.id.includes(SEPARATOR) ? p.id.split(SEPARATOR)[0] : p.id,
                 p,
--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,4 @@ export const LAYER_LOGOS_MARKER = "logos";
 export const LAYER_ICONS_MARKER = "icons";
 export const STRING_META_DELIMITER = "$$";
 export const KIOSK_KEY = "kiosk";
+export const BOOKMARK_PAINTER_MARKER = "booth-bookmark";
