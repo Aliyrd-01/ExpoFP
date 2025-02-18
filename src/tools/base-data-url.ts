7efd96f5ce9a92539bb71f966bdbd21c140ada67
kda <mail@kda.name>
2025-02-17 21:33:08 -0600

    feat: version.js

--- src/tools/base-data-url.ts
+++ src/tools/base-data-url.ts
@@ -1,5 +1,5 @@
 const baseUrl = Array.from(document.getElementsByTagName("script"))
     .filter((x) => x.src.indexOf("/data.js") > -1)[0]
-    .src.replace(/data\.js(\?v=\d{1,3})?$/, "");
+    .src.replace(/data\.js(\?v=\d+)?$/, "");
     
 export default baseUrl;
