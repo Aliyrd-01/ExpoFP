6e3dcb1d925af7843a01eb599057dedff088421e
Evgeny <evgeny.tukin@gmail.com>
2025-02-19 11:20:32 +0300

    fix baseUrl

--- src/tools/base-data-url.ts
+++ src/tools/base-data-url.ts
@@ -1,5 +1,5 @@
 const baseUrl = Array.from(document.getElementsByTagName("script"))
     .filter((x) => x.src.indexOf("/data.js") > -1)[0]
-    .src.replace(/\/data\.js(\?.*)?$/, "");
+    .src.replace(/\/data\.js(\?.*)?$/, "/");
 
 export default baseUrl;
