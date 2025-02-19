b08b67d3dd55f98a6fe09f26eb010a8332ae4ef0
Evgeny <evgeny.tukin@gmail.com>
2025-02-19 11:01:14 +0300

    fix version append

--- src/expofp.tsx
+++ src/expofp.tsx
@@ -16,6 +16,9 @@ ready(async () => {
 
 export const FloorPlan = FloorPlanLoader;
 
+// ET: temp
+window["__fpDataVersion"] = "";
+window["__tempFix"] = true;
 /*
 floorplan loader
     requires all resources
