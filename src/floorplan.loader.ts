d5a73ce8e7d1561297e49bb9d2c951ae9af9057e
kda <mail@kda.name>
2025-02-24 09:15:37 -0600

    fix: EFP-4596

--- src/floorplan.loader.ts
+++ src/floorplan.loader.ts
@@ -327,10 +327,12 @@ export default class FloorPlanLoader implements FloorPlan {
                     retainRebookingToken(token);
                     const url = buildRebookingUrl("api/rebooking-data", token);
                     const resp = await fetch(url);
-                    const rebookingData = await resp.json();
-                    mergeExhibitors(data as Data, rebookingData as Data);
+                    if (resp.ok) {
+                        const rebookingData = await resp.json();
+                        mergeExhibitors(data as Data, rebookingData as Data);
+                        data.isRebooking = Boolean(token);
+                    }
                 }
-                data.isRebooking = Boolean(token);
             } catch (error) {
                 console.error(error);
             }
