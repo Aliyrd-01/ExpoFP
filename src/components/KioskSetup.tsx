cdbec3b74268e73f4b49095dd2b0a43cc29d7ae1
kda <mail@kda.name>
2025-03-06 17:56:49 -0600

    feat: EFP-4586

--- src/components/KioskSetup.tsx
+++ src/components/KioskSetup.tsx
@@ -1,14 +1,13 @@
 import { observer } from "mobx-react-lite";
 import Alert from "./Alert";
 import Button from "./Button";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useCallback, useEffect, useMemo, useState } from "react";
 import store from "../store";
 import { t } from "../utils/i18n";
-import { reaction } from "mobx";
+import { reaction, toJS } from "mobx";
 import Modal from "./Modal";
 import { strEqual } from "../utils/strEqual";
 import { KIOSK_ID_KEY, KIOSK_SETUP_KEY } from "../constants";
-import { CurrentPosition } from "../store/RouteStore";
 import { RouteCutIn } from "../RouteCutIn";
 
 const MODAL_SHOWN_KEY = "kiosk_setup_modal_shown";
@@ -59,31 +58,56 @@ const KioskSetup = observer(() => {
         },
     );
 
+    const requestUrl = useMemo(() => {
+        const url = new URL("/api/kiosks", "https://app.expofp.com/");
+        url.searchParams.set("expoKey", store.fp.eventId);
+        return url.toString();
+    }, [store.fp]);
+
     useEffect(() => {
-        function decodeKioskId(id: string): { x: number; y: number; z: string } {
-            const kioskId = id.split("_") || [];
-            return {
-                x: parseFloat(kioskId[0]),
-                y: parseFloat(kioskId[1]),
-                z: kioskId[2],
-            };
-        }
+        async function requestKioskData() {
+            try {
+                let kioskEncodedId;
 
-        const searchParams = new URLSearchParams(decodeURIComponent(window.location.search));
+                const searchParams = new URLSearchParams(decodeURIComponent(window.location.search));
 
-        if (searchParams.has(KIOSK_SETUP_KEY)) {
-            const kioskSetup = searchParams.get(KIOSK_SETUP_KEY);
-            if (kioskSetup) {
-                store.uiState.kioskSetupData = decodeKioskId(kioskSetup);
+                if (searchParams.has(KIOSK_SETUP_KEY)) {
+                    kioskEncodedId = searchParams.get(KIOSK_SETUP_KEY);
+                    store.uiState.kioskSetup = true;
+                } else if (searchParams.has(KIOSK_ID_KEY)) {
+                    kioskEncodedId = searchParams.get(KIOSK_ID_KEY);
+                }
+
+                if (!kioskEncodedId) {
+                    return;
+                }
+
+                const response = await fetch(requestUrl);
+
+                if (!response.ok) {
+                    setShowError(true);
+                    return;
+                }
+
+                const kiosks = await response.json();
+
+                const kiosk = kiosks.find(k => strEqual(k.key, kioskEncodedId));
+                if (!kiosk) {
+                    return;
+                }
+
+                store.uiState.kioskSetupData = {
+                    x: kiosk.x,
+                    y: kiosk.y,
+                    z: kiosk.layer,
+                };
+            } catch (error) {
+                setShowError(true);
+                return;
             }
-            store.uiState.kioskSetup = true;
         }
-
-        const kioskId = searchParams.get(KIOSK_ID_KEY);
-        if (kioskId) {
-            store.uiState.kioskSetupData = decodeKioskId(kioskId);
-        }
-    }, []);
+        requestKioskData();
+    }, [requestUrl]);
 
     useEffect(() => {
         if (!store.uiState.kioskSetup) {
@@ -108,25 +132,55 @@ const KioskSetup = observer(() => {
         }
     }, [store.uiState.kioskSetup, store.fp.onGetCoordsClick]);
 
-    const save = () => {
-        setShowError(false);
+    async function save() {
+        try {
+            setShowError(false);
 
-        const params = new URLSearchParams(decodeURIComponent(window.location.search));
-        params.delete(KIOSK_SETUP_KEY);
+            const params = new URLSearchParams(decodeURIComponent(window.location.search));
 
-        function encodeKioskId(data: CurrentPosition): string {
-            const { x, y, z } = data || {};
-            return `${x}_${y}_${z}`;
+            const kioskSetupData = toJS(store.uiState.kioskSetupData)
+            const requestBody: {
+                x: number,
+                y: number,
+                z: string,
+                key?: string,
+            } = {
+                x: kioskSetupData.x,
+                y: kioskSetupData.y,
+                z: kioskSetupData.z?.toString(),
+            };
+
+            if (params.get(KIOSK_SETUP_KEY)) {
+                requestBody.key = params.get(KIOSK_SETUP_KEY);
+            }
+
+            const response = await fetch(
+                requestUrl,
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(requestBody),
+                },
+            );
+
+            if (!response.ok) {
+                setShowError(true);
+                return;
+            }
+
+            const kiosk = await response.json();
+
+            params.set(KIOSK_ID_KEY, kiosk.id);
+            window.history.replaceState({}, "", `?${params.toString()}`);
+
+            store.uiState.kioskSetup = false;
+            store.uiState.kiosk = true;
+        } catch (err) {
+            setShowError(true);
         }
+    }
 
-        params.set(KIOSK_ID_KEY, encodeKioskId(store.uiState.kioskSetupData));
-        window.history.replaceState({}, "", `?${params.toString()}`);
-
-        store.uiState.kioskSetup = false;
-        store.uiState.kiosk = true;
-    };
-
-    const exit = () => {
+    function exit() {
         store.uiState.kioskSetup = false;
         sessionStorage.removeItem(MODAL_SHOWN_KEY);
 
@@ -143,66 +197,72 @@ const KioskSetup = observer(() => {
         window.history.replaceState({}, "", `?${params.toString()}`);
     };
 
-    return store.uiState.kioskSetup && (
+    return (
         <Suspense fallback={null}>
-            <Modal open={showGuide} onClickClose={() => setShowGuide(false)}>
-                <h2>{t("Setting up the kiosk")}</h2>
+            {store.uiState.kioskSetup && (
+                <>
+                    <Modal open={showGuide} onClickClose={() => setShowGuide(false)}>
+                        <h2>{t("Setting up the kiosk")}</h2>
 
-                <p>{t("Click on the desired location on the map where the kiosk should be placed.")}</p>
-                <p>{t("The coordinates will be set automatically, and the kiosk will appear at the selected point.")}</p>
-                <p>{t("If needed, adjust the position by clicking on a different location on the map.")}</p>
+                        <p>{t("Click on the desired location on the map where the kiosk should be placed.")}</p>
+                        <p>{t("The coordinates will be set automatically, and the kiosk will appear at the selected point.")}</p>
+                        <p>{t("If needed, adjust the position by clicking on a different location on the map.")}</p>
 
-                <div style={{ textAlign: "right" }}>
-                    <Button
-                        inline
-                        text={t("Ok, got it.")}
-                        onClick={() => {
-                            sessionStorage.setItem(MODAL_SHOWN_KEY, "1");
-                            setShowGuide(false);
-                        }}
-                    />
-                </div>
-            </Modal>
+                        <div style={{ textAlign: "right" }}>
+                            <Button
+                                inline
+                                text={t("Ok, got it.")}
+                                onClick={() => {
+                                    sessionStorage.setItem(MODAL_SHOWN_KEY, "1");
+                                    setShowGuide(false);
+                                }}
+                            />
+                        </div>
+                    </Modal>
 
-            {!showGuide && (<div style={{
-                position: "fixed",
-                bottom: "1rem",
-                left: "50%",
-                transform: "translateX(-50%)",
-                zIndex: 9998,
-            }}>
-                <Alert
-                    variant="blank"
-                    title={t("Setting the position of this kiosk")}
-                    inline
-                    showIcon={false}
-                >
-                    <div
-                        style={{
-                            display: "grid",
-                            gridAutoFlow: "column",
-                            gap: "0.5rem",
-                            paddingTop: "1rem",
-                        }}
-                    >
-                        <Button 
-                            inline 
-                            size="sm" 
-                            text={t("Save")} 
-                            disabled={!store.uiState.kioskSetupData} 
-                            onClick={save}
-                        />
+                    {!showGuide && (
+                        <div style={{
+                            position: "fixed",
+                            bottom: "1rem",
+                            left: "50%",
+                            transform: "translateX(-50%)",
+                            zIndex: 9998,
+                        }}>
+                            <Alert
+                                variant="blank"
+                                title={t("Setting the position of this kiosk")}
+                                inline
+                                showIcon={false}
+                            >
+                                <div
+                                    style={{
+                                        display: "grid",
+                                        gridAutoFlow: "column",
+                                        gap: "0.5rem",
+                                        paddingTop: "1rem",
+                                    }}
+                                >
+                                    <Button
+                                        inline
+                                        size="sm"
+                                        text={t("Save")}
+                                        disabled={!store.uiState.kioskSetupData}
+                                        onClick={save}
+                                    />
 
-                        <Button 
-                            variant="gray" 
-                            size="sm" 
-                            inline
-                            text={t("Exit")}
-                            onClick={exit}
-                        />
-                    </div>
-                </Alert>
-            </div>)}
+                                    <Button
+                                        variant="gray"
+                                        size="sm"
+                                        inline
+                                        text={t("Exit")}
+                                        onClick={exit}
+                                    />
+                                </div>
+                            </Alert>
+                        </div>
+                    )}
+                </>
+            )}
 
             {showError && (
                 <div style={{
@@ -219,7 +279,7 @@ const KioskSetup = observer(() => {
                         inline
                         onClose={() => setShowError(false)}
                     >
-                        {t("An error occurred while setting the kiosk position.\nPlease try again.")}
+                        {t("An error occurred.\nPlease try again.")}
                     </Alert>
                 </div>
             )}
