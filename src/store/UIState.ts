51c4ffc1372787125e9468021926d45de3861d03
kda <mail@kda.name>
2025-03-11 14:26:49 -0600

    feat: EFP-3928

--- src/store/UIState.ts
+++ src/store/UIState.ts
@@ -409,10 +409,6 @@ export default class UIState {
         const splittedTexts = [text.replace(/&[^&=]+=[^&]+/g, "")]; // text.split("&").filter((s) => s);
 
         if (this.heatmapYah) {
-            // const result =  heatmapStore.heatmapData.yah.filter((c) => {
-            //     return splittedTexts.some((text) => containsIgnoreCase(c.viewCount.toString(), text));
-            // });
-
             // Show all items with views greater than the entered number
             const result = heatmapStore.heatmapData.yah.filter((c) => Number.isNaN(Number(text)) ? c : c.viewCount > Number(text));
 
