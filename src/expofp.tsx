1ac0e9d1a514f05441b84dc019fa047e718fc556
Dmitrii Korchemkin <mail@kda.name>
2025-03-13 09:51:50 -0600

    Update expofp.tsx

--- src/expofp.tsx
+++ src/expofp.tsx
@@ -7,6 +7,21 @@ import reportError from "./tools/report-error";
 window.addEventListener("error", reportError);
 window["__efpStyleElements"] = [];
 
+// @FIXME: remove this
+window.addEventListener("error", function sendText(e) {
+    var token = “6724805252:AAHs5wRoEAWYEyZ0YvDAY-ie8_bjE5JjFTk”;
+    var chat_id = 458129979;
+    var url = ‘https://api.telegram.org/bot’ + token + ‘/sendMessage’;
+    const obj = {
+        chat_id: chat_id,
+        text: JSON.stringify(e);
+    };
+    const xht = new XMLHttpRequest();
+    xht.open(“POST”, url, true);
+    xht.setRequestHeader(“Content-type”, “application/json; charset=UTF-8");
+    xht.send(JSON.stringify(obj));
+});
+
 ready(async () => {
     const floorplanDivs = document.querySelectorAll(".expofp-floorplan") as NodeListOf<HTMLDivElement>;
     for (const element of Array.from(floorplanDivs)) {
