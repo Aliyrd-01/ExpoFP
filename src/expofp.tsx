7cc0c717dd916625967ea449588babe0f1b068ba
kda <mail@kda.name>
2025-03-13 09:59:42 -0600

    fix: listener

--- src/expofp.tsx
+++ src/expofp.tsx
@@ -9,17 +9,20 @@ window["__efpStyleElements"] = [];
 
 // @FIXME: remove this
 window.addEventListener("error", function sendText(e) {
-    var token = “6724805252:AAHs5wRoEAWYEyZ0YvDAY-ie8_bjE5JjFTk”;
-    var chat_id = 458129979;
-    var url = ‘https://api.telegram.org/bot’ + token + ‘/sendMessage’;
-    const obj = {
-        chat_id: chat_id,
-        text: JSON.stringify(e);
-    };
-    const xht = new XMLHttpRequest();
-    xht.open(“POST”, url, true);
-    xht.setRequestHeader(“Content-type”, “application/json; charset=UTF-8");
-    xht.send(JSON.stringify(obj));
+    try {
+        let token = "6724805252:AAHs5wRoEAWYEyZ0YvDAY-ie8_bjE5JjFTk";
+        let url = "https://api.telegram.org/bot" + token + "/sendMessage";
+        const obj = {
+            chat_id: 458129979,
+            text: JSON.stringify(e),
+        };
+        const xht = new XMLHttpRequest();
+        xht.open("POST", url, true);
+        xht.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        xht.send(JSON.stringify(obj));
+    } catch (e) {
+        console.error(e);
+    }
 });
 
 ready(async () => {
