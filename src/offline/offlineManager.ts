66c28bea1ca474c69d1a5809e0d915b3d2c387b3
kda <mail@kda.name>
2025-03-28 16:15:18 -0600

    feat: Automatic activation of the service worker for kiosk mode was enabled

--- src/offline/offlineManager.ts
+++ src/offline/offlineManager.ts
@@ -1,15 +1,26 @@
+import isWebview from "../utils/is-webview";
 import { BROADCAST_CHANNEL_NAME, MESSAGE_CACHE, MESSAGE_CACHE_BUNDLE, MESSAGE_REFRESH } from "./constants";
 
-export async function initOfflineManager(baseUrl: string, resourceUrls: string[]): Promise<void> {
+export async function initOfflineManager(
+    baseUrl: string,
+    resourceUrls: string[],
+    activationKeys = [],
+): Promise<void> {
     try {
-        if (!("serviceWorker" in navigator)) {
+        if (!("serviceWorker" in navigator) || isWebview) {
             return;
         }
 
-        const command = new URLSearchParams(window.location.search).get("sw");
+        const searchParams = new URLSearchParams(window.location.search);
+        const command = searchParams.get("sw");
         const scope = "/";
 
-        if (command === "1") {
+        const hasActivationKey = activationKeys.some(k => {
+            const value = searchParams.get(k);
+            return value && value !== "0";
+        });
+
+        if (command === "1" || hasActivationKey) {
             await navigator.serviceWorker.register(buildUrl("sw.js"), { scope });
             await navigator.serviceWorker.ready;
         } else if (command === "0") {
