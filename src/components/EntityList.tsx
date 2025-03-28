0bacddef60a2422e8eafab0593aae175cb337d0a
kda <mail@kda.name>
2025-03-28 14:52:21 -0600

    feat: clear highlighting

--- src/components/EntityList.tsx
+++ src/components/EntityList.tsx
@@ -30,6 +30,16 @@ function EntityList({ updatedScrollableRef, updateScroll }: ListProps) {
         if (el) el.scrollIntoView({ block: "nearest", inline: "nearest" });
     }, [store.layerStore.layersLoaded]);
 
+    const clearHighlightingTimer = useRef(null);
+    function clearHighlighting() {
+        clearTimeout(clearHighlightingTimer.current);
+        clearHighlightingTimer.current = setTimeout(() => {
+            if (!uiState.details) {
+                uiState.clearListScrollItemId();
+            }
+        }, 2500);
+    }
+
     function handleClick(type: EntityItemType, data: string) {
         const id = parseInt(data);
 
@@ -150,13 +160,7 @@ function EntityList({ updatedScrollableRef, updateScroll }: ListProps) {
                     customScrollParent={scrollableRef.current}
                     totalCount={uiState.listItems.length}
                     initialTopMostItemIndex={uiState.listScrollIndex}
-                    isScrolling={() => {
-                        setTimeout(() => {
-                            if (!uiState.details) {
-                                uiState.clearListScrollItemId();
-                            }
-                        }, 2000);
-                    }}
+                    isScrolling={() => clearHighlighting()}
                 />
             )}
         </div>
