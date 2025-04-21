0fdb6d0f56bb02a96506090aaa362971536492ae
kda <mail@kda.name>
2025-04-21 17:41:44 -0600

    feat(EFP-4704): track kiosk

--- src/tools/track-event.ts
+++ src/tools/track-event.ts
@@ -3,6 +3,7 @@ import logger from "./logger";
 import { yahKey } from "../utils/yah"
 import { getLocationHistory } from "../services/routing";
 import { uiState } from "../store";
+import { KIOSK_ID_KEY } from "../constants";
 
 export default function trackEvent(type: "load" | "exview" | "search" | "route" | "share" | "booview" | "catview", value?: any) {
     logger.log("trackEvent", type, value);
@@ -20,8 +21,12 @@ export default function trackEvent(type: "load" | "exview" | "search" | "route" 
         const prevLocation = history[history.length - 2];
         const Xref = prevLocation ? window.location.origin + prevLocation : null;
 
+        const searchParams = new URLSearchParams(decodeURIComponent(window.location.search));
+        const kioskId = searchParams.get(KIOSK_ID_KEY);
+
         const headers = {
-            "X-href": `${window.location.href}${!!yah ? `#${encodeURIComponent(yah)}` : ""}`
+            "X-href": `${window.location.href}${!!yah ? `#${encodeURIComponent(yah)}` : ""}`,
+            ...(kioskId ? { "X-kiosk-id": kioskId } : {}),
         }
 
         if (Xref) {
