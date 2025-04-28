adbc59f977dffb825e04ef6d7a6e12ec8738ce1e
vladimir-n0691 <140501809+vladimir-n0691@users.noreply.github.com>
2025-04-28 20:41:56 +0300

    Update floorplan.ready.tsx

--- src/floorplan.ready.tsx
+++ src/floorplan.ready.tsx
@@ -169,8 +169,8 @@ export default class FloorPlanReady extends FloorPlanLoader {
     }    
 
     selectCurrentPosition(point: CurrentPosition, focus: boolean, icon?: number): void {
-        if (point?.angle != null && __fpGeo?.properties?.bearing != null) {
-            point.angle = 90.0 + __fpGeo.properties.bearing - (point.angle % 360.0);
+        if (point?.angle != null && fpGeo?.properties?.bearing != null) {
+            point.angle = 90.0 + fpGeo.properties.bearing - (point.angle % 360.0);
             point.angle = point.angle < 0 ? point.angle + 360.0 : point.angle;
             point.angle = point.angle > 360 ? point.angle - 360.0 : point.angle;
         }
