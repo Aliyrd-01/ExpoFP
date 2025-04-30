3a1a811209d449ac5412a6f9fe6f985a8f433a50
kda <mail@kda.name>
2025-04-30 12:43:27 -0600

    fix: image slider width

--- src/components/MarketMaterialList.tsx
+++ src/components/MarketMaterialList.tsx
@@ -9,12 +9,12 @@ export type MarketMaterialListProps = {
 const MarketMaterialListItem: React.FC<MarketMaterial> = ({ fileName, path }) => {
     return (
         <a className="market-materials__item" href={path} target="_blank" rel="noopener noreferrer">
-            <div className="market-materials__item-name">
-                <div className="market-materials__item-file">
-                    <i className="icon-file-solid"></i>
-                </div>
-                <span>{fileName}</span>
+            <div className="market-materials__item-file">
+                <i className="icon-file-solid"></i>
             </div>
+
+            <div className="market-materials__item-name">{fileName}</div>
+
             <div className="market-materials__item-download">
                 <i className="icon-download"></i>
             </div>
