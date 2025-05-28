f24fa37cfdf479e5abfbadef365f2760a8e77b12
kda <mail@kda.name>
2025-05-28 15:14:36 +0300

    feat: EFP-4747 fuzzy search settings

--- src/store/FuzzySearchEngineStore.ts
+++ src/store/FuzzySearchEngineStore.ts
@@ -9,7 +9,7 @@ export default class FuzzySearchEngineStore {
             const Fuse = FuseModule.default;
 
             this.engine = new Fuse([], {
-                keys: ["name", "layer.name", "description", "title", "fullName"],
+                keys: ["name", "layer.name"],
                 threshold: 0.45,
                 ignoreLocation: true,
             });
