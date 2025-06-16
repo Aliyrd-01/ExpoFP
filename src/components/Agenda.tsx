2cb3a88eda65028f0c3f096d34affb385e135548
Pashkella <hryshkodev@gmail.com>
2025-06-16 17:33:32 +0300

    refactor(Agenda): fix sticky position with filters row

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -173,7 +173,13 @@ const Agenda: React.FC<AgendaProps> = observer(({ showFilters = true }) => {
                 )}
 
                 {sortedEvents.length > 0 ? (
-                    <Schedule events={sortedEvents} showMoreButton={false} showBooths={true} onEventClick={handleEventClick} />
+                    <Schedule
+                        events={sortedEvents}
+                        showMoreButton={false}
+                        showBooths={true}
+                        isAgenda={true}
+                        onEventClick={handleEventClick}
+                    />
                 ) : (
                     <div className="efp-agenda-empty">
                         {store.agendaFilterStore.activeFiltersCount > 0 || localStore.searchValue
