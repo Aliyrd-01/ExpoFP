3e004bd07dcfc9f2009d81712946ba868d1a7bf1
kda <mail@kda.name>
2025-06-18 10:39:28 +0300

    test

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -206,22 +206,22 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
             // if (s.currentTop === newTop) return;
             const $el = select(el.current);
             $el.interrupt();
-            // if (transition) {
-            //     $el.transition()
-            //         .ease(easePolyOut)
-            //         .duration(500)
-            //         .style("top", newTop + "px")
-            //         .on("end", () => {
-            //             if (uiState.overlaySize === "full") {
-            //                 const containerHeight =
-            //                     el.current?.parentElement?.getBoundingClientRect?.()?.height || window.innerHeight;
-            //                 el.current.style.height = `${containerHeight - newTop}px`;
-            //             }
-            //             setShowAll();
-            //         });
-            // } else {
+            if (transition) {
+                $el.transition()
+                    .ease(easePolyOut)
+                    .duration(500)
+                    .style("top", newTop + "px")
+                    .on("end", () => {
+                        if (uiState.overlaySize === "full") {
+                            const containerHeight =
+                                el.current?.parentElement?.getBoundingClientRect?.()?.height || window.innerHeight;
+                            el.current.style.height = `${containerHeight - newTop}px`;
+                        }
+                        setShowAll();
+                    });
+            } else {
                 el.current.style.top = newTop + "px";
-            // }
+            }
             setShowAll();
 
             if (s.currentTop !== newTop && window.event) window.event.preventDefault();
