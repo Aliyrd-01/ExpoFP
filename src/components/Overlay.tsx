215d70b636a8f90345ba501ebd02ac1ef6a0f8de
kda <mail@kda.name>
2025-06-18 12:46:42 +0300

    test

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -189,7 +189,7 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
 
             // height depends on size and ongoing touch
             // let's animate when no touch in progress
-            // if (uiState.overlayPosition === "left") return;
+            if (uiState.overlayPosition === "left") return;
 
             let newTop = getTopForBottomPosition(uiState.overlaySize, el.current);
 
@@ -203,7 +203,7 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
             } else if (s.currentTop === undefined) {
                 transition = false;
             }
-            // if (s.currentTop === newTop) return;
+            if (s.currentTop === newTop) return;
             const $el = select(el.current);
             $el.interrupt();
             if (transition) {
@@ -214,7 +214,7 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
                     .on("end", () => {
                         if (uiState.overlaySize === "full") {
                             const containerHeight =
-                                /*el.current?.parentElement?.getBoundingClientRect?.()?.height ||*/ window.innerHeight;
+                                el.current?.parentElement?.getBoundingClientRect?.()?.height || window.innerHeight;
                             el.current.style.height = `${containerHeight - newTop}px`;
                         }
                         setShowAll();
@@ -224,7 +224,7 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
             }
             setShowAll();
 
-            // if (s.currentTop !== newTop && window.event) window.event.preventDefault();
+            if (s.currentTop !== newTop && window.event) window.event.preventDefault();
             s.currentTop = newTop;
         }
 
@@ -272,7 +272,7 @@ export default observer(function Overlay({ isGDPR, allowConsent }: OverlayProps)
 const miniSizeRems = 3.5;
 const paddingRems = 2;
 function getTopForBottomPosition(size: OverlaySize, el: HTMLDivElement): number {
-    const containerHeight = /*el?.parentElement?.getBoundingClientRect?.()?.height ||*/ window.innerHeight;
+    const containerHeight = el?.parentElement?.getBoundingClientRect?.()?.height || window.innerHeight;
     switch (size) {
         case "full":
             return remsToPixels(paddingRems);
