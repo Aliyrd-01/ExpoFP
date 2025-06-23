bc15fc5abb9fbe5e956e69d8db2228f9724cee61
kda <mail@kda.name>
2025-06-23 18:28:41 +0300

    feat: EFP-4819

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -262,7 +262,7 @@ export default function Map() {
     function moveToRect(rect: Rect, maxZoomScale: number = 10, animate: boolean = true) {
         let newRect = Rect.fromX1y1x2y2(rect.x1 - uiState.kioskRectPadding * rect.w, rect.y1, rect.x2, rect.y2);
 
-        // if (settings.EXPO === "springfair2022") maxZoomScale = 20;
+        if (settings.EXPO === "springfair2022") maxZoomScale = 20;
         const zoomScale = zoomTransform(s.$canvas.node()).k; //m.getZoomTransform().k;
 
         let visibleRect = uiState.canvasVisibleRectPx;
