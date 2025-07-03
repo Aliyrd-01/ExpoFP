646ac8343d7a780387894a4919a535f7d46408c2
kda <mail@kda.name>
2025-07-03 18:13:06 +0300

    feat: debug query

--- src/tools/handleDebugMode.ts
+++ src/tools/handleDebugMode.ts
@@ -2,20 +2,38 @@ import { DEBUG_KEY } from "../constants";
 
 export function handleDebugMode() {
     try {
-        const url = new URL(decodeURIComponent(window.location.href));
-        const debug = url.searchParams.get(DEBUG_KEY);
+        const url = new URL(window.location.href);
+        let debugValue = null;
+        let paramToDelete = null;
 
-        if (debug) {
-            if (debug === "1") {
-                localStorage.setItem(DEBUG_KEY, "1");
-            } else if (debug === "0") {
-                localStorage.removeItem(DEBUG_KEY);
+        for (const [key, value] of url.searchParams) {
+            if (key === DEBUG_KEY) {
+                // ?debug=1
+                debugValue = value;
+                paramToDelete = key;
+                break;
+            } else if (key.startsWith(DEBUG_KEY + "%3D") || key.startsWith(DEBUG_KEY + "=")) {
+                // ?debug%3D1
+                debugValue = key.split("=")[1];
+                paramToDelete = key;
+                break;
             }
+        }
 
-            url.searchParams.delete(DEBUG_KEY);
-            window.location.href = url.toString();
+        if (debugValue === "1") {
+            localStorage.setItem(DEBUG_KEY, "1");
+        } else if (debugValue === "0") {
+            localStorage.removeItem(DEBUG_KEY);
+        } else {
+            return;
+        }
+
+        if (paramToDelete) {
+            url.searchParams.delete(paramToDelete);
+            window.history.replaceState(null, "", url.toString());
+            window.location.reload();
         }
     } catch (err) {
-        console.error(err);
+        console.error("Error in handleDebugMode:", err);
     }
 }
